2315f4fce1cf51c8c6451e67d30e46e311ea7af7
Alex <andres.a.campos@navy.mil>
2022-07-04 21:15:27 -0700

    adding some dafault nodes data

---
+++ src/components/DefaultNodes.js
@@ -0,0 +1,23 @@
+let nodes = [
+    {
+        title:"Have Pathological Self Confidence & Humility",
+        content:"The danger becomes that the huminity either overwhelms the ego and the drive, or that the self confidence and ego extinguish the humility.",
+        inspiration:"Eric Weinstein"
+    },
+    {
+        title:"You Owe The World Your Struggle, Not Your Sucess.",
+        content:"You are resposible for struggling, so struggle mightily. It is not the world's job to do your struggle for you. But, if you've tried your best; If you've struggled, and still failed, forgive yourself. Beacuase the world does not owe you its forgiveness. You owe it to yourself to forgive yourself.",
+        inspiration:"Eric Weinstein"
+    },
+    {
+        title:"Be A First Class Noticer",
+        content:"Become the type of person on whom nothing is lost, by changing the way you look at things. Because if you change the way you look at things, the things you look at change.",
+        inspiration:"Mark Goulston, Amy Herman"
+    },
+    {
+        title:"You Owe The World Your Struggle, Not Your Sucess.",
+        content:"You are resposible for struggling so struggle mightily.I",
+        inspiration:"Eric Weinstein"
+    },
+
+]
