e66f91c90565ae4eafb8ec65e9bf085d88095c3e
Alex <andres.a.campos@navy.mil>
2022-08-10 21:07:24 -0700

    adding styled component to node card to give the glass blur aesthetic

--- src/components/NodeCard.js
+++ src/components/NodeCard.js
@@ -1,17 +1,42 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import styled from 'styled-components'
 
 export default function NodeCard(props) {
   const deleteNodeCard = () => {
     props.onDelete(props.id);
   };
   return (
-    <div className="note">
+    <DivNodeCard>
       <button onClick={deleteNodeCard}>
         <DeleteIcon />
       </button>
       <h1>{props.title} </h1>
       <p> {props.content} </p>
-    </div>
+    </DivNodeCard>
   );
 }
+
+let DivNodeCard = styled.div `
+  background: #00219708;
+  border-radius: 5px;
+  box-shadow: 0px 0px 4px #ccc;
+  padding: 10px;
+  width: 150px;
+  margin: 10px;
+  backdrop-filter: blur(5px);
+  
+  color: ${(props) => (props.primary ? 'hotpink' : 'turquoise')}
+
+  h1 {
+    font-size: 1.2em;
+    margin: 6px 0;
+  }
+
+  p {
+    font-size: 1.1em;
+    margin: 6px 0;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
+`
