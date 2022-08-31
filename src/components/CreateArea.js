183a9f5e6bc8f086dd6fe6fa9eac7d30a8912358
Alex <andres.a.campos@navy.mil>
2022-08-30 22:59:03 -0700

    styling create area

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -3,6 +3,7 @@ import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
 import _ from "lodash";
+import styled from "@emotion/styled";
 
 function CreateArea(props) {
   const [note, setNote] = useState({ title: "", content: "" });
@@ -28,7 +29,7 @@ function CreateArea(props) {
     setIsExpanded(true);
   }
   return (
-    <div>
+    <StyledCreateArea id='create-area'>
       <form className="create-note">
         {isExpanded && (
           <input
@@ -53,8 +54,48 @@ function CreateArea(props) {
           </Fab>
         </Zoom>
       </form>
-    </div>
+    </StyledCreateArea>
   );
 }
+
+let StyledCreateArea = styled.div`
+  float: left;
+  
+  form.create-note {
+    width: 300px;
+    margin: 30px 0 20px 0;
+    background: #fcfcfc;
+    padding: 15px;
+    border-radius: 7px;
+    box-shadow: 0 1px 5px rgb(138, 137, 137);
+  }
+  form.create-note input,
+  form.create-note textarea {
+    background-color: #fcfcfc;
+    width: 100%;
+    border: none;
+    padding: 4px;
+    outline: none;
+    font-size: 1.2em;
+    font-family: inherit;
+    resize: none;
+  }
+  form.create-note button {
+    position: absolute;
+    right: 12px;
+    bottom: -18px;
+    background: #1313f588;
+    backdrop-filter: blur(5px);
+    color: #fff;
+    border: 1px solid black;
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    outline: none;
+  }
+
+`
 
 export default CreateArea;
