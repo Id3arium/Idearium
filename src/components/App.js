bb2f87392e51c099ce55243803f765162b0eca46
Alex C <andres.al.campos@gmail.com>
2022-09-02 01:27:43 -0700

    adding new node function to handle adding nodes from the idea composition area

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
 //import ForceGraph3d from './components/ForceGraph3D';
 import CreateArea from "./CreateArea.js";
 import styled from "styled-components";
 import NodeCardsArea from "./NodeCardsArea.js";
-import nodes from "./nodes.json";
+import defaultNodes from "./nodes.json";
+import zustand from "zustand";
 
 function App() {
-  let gData = () => {
-    // Random tree
-    const clusterSize = 100;
-    return {
-      nodes: [...Array(clusterSize).keys()].map((i) => ({ id: i })),
-      links: [...Array(clusterSize).keys()]
-        .filter((id) => id)
-        .map((id) => ({
-          source: id,
-          target: Math.round(Math.random() * (id - 1)),
-        })),
-    };
-  };
+	let [nodes,setNodes] = useState(defaultNodes)
+  	let gData = () => {
+		// Random tree
+		const clusterSize = 100;
+		return {
+		nodes: [...Array(clusterSize).keys()].map((i) => ({ id: i })),
+		links: [...Array(clusterSize).keys()]
+			.filter((id) => id)
+			.map((id) => ({
+			source: id,
+			target: Math.round(Math.random() * (id - 1)),
+			})),
+		};
+	};
 
-  useEffect(() => {}, []);
+	function addNode(newNode){
+		let newNodeData = {
+			id: nodes.length,
+			title: newNode.title,
+			content: newNode.content,
+			inspiration: newNode.inspiration
+		}
+		setNodes([...nodes,newNodeData])
+	}
 
-  console.log(gData());
-  return (
-    <StyledApp id="App">
-      <div className="force-graph">
-        <ForceGraph3D graphData={gData()} width={850} />
-      </div>
-      <div>
-        <CreateArea />
-        <NodeCardsArea nodes={nodes} />
-      </div>
-    </StyledApp>
-  );
+  	useEffect(() => {}, []);
+
+	console.log(gData());
+	return (
+		<StyledApp id="App">
+			<div className="force-graph">
+				<ForceGraph3D graphData={gData()} width={850} />
+			</div>
+			<div>
+				<CreateArea onAdd={addNode} />
+				<NodeCardsArea nodes={nodes} />
+			</div>
+		</StyledApp>
+	);
 }
 
 export default App;
