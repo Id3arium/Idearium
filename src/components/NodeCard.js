d9e1365f674b506ccc3dbd06f83440f79eebe46d
Alex C <andres.al.campos@gmail.com>
2022-09-15 00:55:22 -0700

    adding timer bar and animations that work

--- src/components/NodeCard.js
+++ src/components/NodeCard.js
@@ -1,37 +1,29 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { IconButton } from "@mui/material";
 import styled from "styled-components";
-import { useNodesStore, useNodesTimelineStore } from "../Store.js";
-import { useSpring,animated } from "react-spring";
+import { useNodesTimelineStore } from "../Store.js";
+import { motion, useAnimationControls } from "framer-motion";
 
 export default function NodeCard(props) {
     const [backSideVisible, setBackSideVisible] = useState(false)
     const [isHovered, setIsHovered] = useState(false)
-    const [duration, setDuration] = useState(props.duration)
     let nodeIDsTimeline = useNodesTimelineStore(state => state.nodeIDsTimeline)
     let currTimelineIdx = useNodesTimelineStore(state => state.currTimelineIdx)
-    let nodes = useNodesStore(state => state.nodes)
-    let nodeData = nodes[props.nodeID]
-    console.log("props.nodeID",props.nodeID)
-    console.log("props.duration",props.duration)
 
-    let timerBarAnim = useSpring(() => ({
-        from: { opacity: .25 },
-        to: { opacity: 1 },
-        config:{duration: Math.max(50, duration)},
-        loop: {
-           reset: true,
-        },
-        onRest: (o) => {
-            console.log("onRest",props.duration)
-            props.onNext()
-            setDuration(props.duration)
-        },
-    }))
+    const animation = useAnimationControls()
+    let initialStyles = { opacity: .25,width: "585px"}
+    let targetStyles = {
+        opacity: .1, 
+        width: "0px",
+        transition: {
+            duration: props.duration,
+            ease: "linear"
+        }
+     }
 
 	function handleClick(e){ 
         if (e.target.id === "node-card"){
@@ -40,41 +32,50 @@ export default function NodeCard(props) {
 	}
 
     const deleteNodeCard = () => {
-        props.onDelete(nodeData.id);
+        props.onDelete(props.nodeData.id);
     };
+
+    function restartCardAnimation(){
+        animation.stop()
+        animation.set(initialStyles)
+        animation.start(targetStyles)
+    }
+    function animateNextCard(){
+        props.onNext()
+        restartCardAnimation()
+    }
+    function animatePrevCard(){
+        props.onPrev()
+        restartCardAnimation()
+    }
 
     useEffect(()=>{
         if (backSideVisible || isHovered){
-            timerBarAnim.pause()
+            animation.stop()
         } else {
-            timerBarAnim.resume()
+            animation.start(targetStyles)
         }
-    },[backSideVisible,isHovered])
-
-    useEffect(()=>{
-        timerBarAnim.start({
-            config: { duration: duration }
-        });
-    },[duration])
+    },[backSideVisible,isHovered,props.duration])
     
     return (
-    <StyledNodeCard as={animated.div} id="node-card" 
-        style={{
-            opacity: timerBarAnim.opacity.to((o) => opacity )
-        }}
- 
+    <StyledNodeCard id="node-card" 
         onClick={handleClick}  
         onMouseEnter={()=>{setIsHovered(true)}} 
         onMouseLeave={()=>{setIsHovered(false)}}
     >
+        <TimerBar as={motion.div} isVisible={!backSideVisible} isHovered={isHovered}
+            animate={animation} 
+            initial={initialStyles}
+            onAnimationComplete={animateNextCard}
+        />
         <div className="card-controls" >
             <IconButton className="nav-btn top left outlined" 
-                onClick={() => {props.onPrev()}}
+                onClick={animatePrevCard}
             >
                 <KeyboardArrowLeftIcon  />
             </IconButton>
             <IconButton className="nav-btn top right outlined" 
-                onClick={() => {props.onNext()}}
+                onClick={animateNextCard}
             >
                 <KeyboardArrowRightIcon disabled={true}/>
             </IconButton>
@@ -94,14 +95,14 @@ export default function NodeCard(props) {
         
         <div className="card-content" >
             <StyledCardSide id="front-side" isVisible={!backSideVisible} isHovered={isHovered}>
-                {nodeData.title && <h1 >{nodeData.title} </h1>}
-                <p>{nodeData.content}</p> 
+                {props.nodeData.title && <h1 >{props.nodeData.title} </h1>}
+                <p>{props.nodeData.content}</p> 
             </StyledCardSide>
             <StyledCardSide id="back-side" isVisible={backSideVisible} isHovered={isHovered}>
                 <h1 > Node #{props.nodeID+1} [{currTimelineIdx+1} / {nodeIDsTimeline.length}] </h1>
-                <p>Inspiration: {nodeData.inspiration}  </p><br></br>
+                <p>Inspiration: {props.nodeData.inspiration}  </p><br></br>
                 <p className="frequency">
-                    {(nodeData.frequency * 100).toFixed(1)}% Likely to appear
+                    {(props.nodeData.frequency * 100).toFixed(1)}% Likely to appear
                 </p>
             </StyledCardSide>
         </div>
@@ -110,7 +111,20 @@ export default function NodeCard(props) {
     );
 }
 
-let StyledCardSide = styled.div`
+const TimerBar = styled.div`
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 0);
+    border-radius: 1px;
+    bottom: 0px;
+    filter: ${props => props.isVisible ? "none": (props.isHovered ? "blur(3px)" : "blur(9px)") };
+    pointer-events: none;
+    height: 5px;
+    margin: 0 auto;
+    background-color: white;
+`
+
+const StyledCardSide = styled.div`
   opacity: ${props => props.isVisible ? "1": ".15"};
   filter: ${props => props.isVisible ? "none": (props.isHovered ? "blur(3px)" : "blur(9px)") };
   transform: ${props => props.isVisible ? "scale(1, 1)": "scale(-1, 1)"};;
@@ -119,10 +133,10 @@ let StyledCardSide = styled.div`
   pointer-events: none;
 `
 
-let StyledNodeCard = styled.div`
+const StyledNodeCard = styled.div`
   background: #00219708;
   border-radius: 5px;
-  box-shadow: 0px 0px 4px #ccc;
+  box-shadow: 0px 0px 4px #CCC;
   padding: 20px 30px 30px;
   width: 525px;
   margin: 4px;
@@ -130,6 +144,7 @@ let StyledNodeCard = styled.div`
   color: #EEE;
   backdrop-filter: blur(5px);
   background-color: #ffffff0C;
+  overflow: visible;
 
   :hover{
     background-color: #ffffff00;
