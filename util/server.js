121855235ef0abdd1ec70ca0e579de8c2f4cc287
Alex C <andres.al.campos@gmail.com>
2022-12-19 11:14:14 -0800

    trying to figure out how to connect to the db

---
+++ util/server.js
@@ -0,0 +1,28 @@
+import next from 'next';
+import { createServer } from 'http';
+import { parse } from 'url';
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handle = app.getRequestHandler();
+
+export default async function connectToDatabase(){
+  app.prepare().then(() => {
+    createServer((req, res) => {
+      // Parse the request url
+      const parsedUrl = parse(req.url, true);
+  
+      // Custom route handler
+      if (parsedUrl.pathname === '/custom-route') {
+        res.end('Hello, world!');
+        return;
+      }
+  
+      // Default route handler
+      handle(req, res, parsedUrl);
+    }).listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
+  });
+} 
+
