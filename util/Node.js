aeff049211d9cec209d59093898c7577a083d6a8
Alex C <andres.al.campos@gmail.com>
2022-12-19 11:13:01 -0800

    adding node schema

---
+++ util/Node.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const NodeSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  title: {
+    type: String,
+    required: false,
+    trim: true,
+    maxlength: [100, "Title can't exceed 100 characters"]
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  inspiration: {
+    type: String,
+    required: [true, "Y no inspiration?"]
+  },
+  frequancy: {
+    type: Number,
+    required: true
+  },
+  ranking: {
+    type: Number,
+    required: true
+  }
+})
+
+const Node = mongoose.models.Node || mongoose.model('Node', NodeSchema)
+
+module.exports = Node
