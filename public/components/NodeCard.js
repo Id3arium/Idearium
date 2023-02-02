b05ad11d0773e08582b3fe57b3c00469a10239db
Alex C <andres.al.campos@gmail.com>
2023-02-01 21:37:45 -0800

    commenting out some code

--- public/components/NodeCard.js
+++ public/components/NodeCard.js
@@ -1,207 +1,207 @@
-import React, { useState, useEffect } from "react";
-import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import { IconButton } from "@mui/material";
-import styled from "styled-components";
-import { useNodesTimelineStore ,useNodeCardStore} from "../Store.js";
-import { motion, useAnimationControls } from "framer-motion";
+// import React, { useState, useEffect } from "react";
+// import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+// import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+// import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+// import { IconButton } from "@mui/material";
+// import styled from "styled-components";
+// import { useNodesTimelineStore ,useNodeCardStore} from "../Store.js";
+// import { motion, useAnimationControls } from "framer-motion";
 
-export default function NodeCard(props) {
+// export default function NodeCard(props) {
 
-    const [isHovered, setIsHovered] = useNodeCardStore(state => [state.isHovered,state.setIsHovered])
-    const [frontSideVisible, setFrontSideVisible] = useNodeCardStore(state => [state.frontSideVisible, state.setFrontSideVisible])
-    const nodeIDsTimeline = useNodesTimelineStore(state => state.nodeIDsTimeline)
-    const currTimelineIdx = useNodesTimelineStore(state => state.currTimelineIdx)
+//     const [isHovered, setIsHovered] = useNodeCardStore(state => [state.isHovered,state.setIsHovered])
+//     const [frontSideVisible, setFrontSideVisible] = useNodeCardStore(state => [state.frontSideVisible, state.setFrontSideVisible])
+//     const nodeIDsTimeline = useNodesTimelineStore(state => state.nodeIDsTimeline)
+//     const currTimelineIdx = useNodesTimelineStore(state => state.currTimelineIdx)
 
-    const animation = useAnimationControls()
-    let initialStyles = { 
-        opacity: .125, 
-        width: "585px",
-    }
-    let targetStyles = {
-        opacity: .15, 
-        width: "0px",
-        transition: {
-            duration: props.duration,
-            ease: "linear"
-        }
-    }
+//     const animation = useAnimationControls()
+//     let initialStyles = { 
+//         opacity: .125, 
+//         width: "585px",
+//     }
+//     let targetStyles = {
+//         opacity: .15, 
+//         width: "0px",
+//         transition: {
+//             duration: props.duration,
+//             ease: "linear"
+//         }
+//     }
 
-	function handleClick(e){ 
-        if (e.target.id === "node-card"){
-            setFrontSideVisible(!frontSideVisible)
-		}
-	}
+// 	function handleClick(e){ 
+//         if (e.target.id === "node-card"){
+//             setFrontSideVisible(!frontSideVisible)
+// 		}
+// 	}
 
-    const deleteNodeCard = () => props.onDelete(props.nodeData.id)
+//     const deleteNodeCard = () => props.onDelete(props.nodeData.id)
 
-    function restartCardAnimation(){
-        animation.stop()
-        animation.set(initialStyles)
-        // animation.start(targetStyles)
-    }
-    function animateNextCard(){
-        props.onNext()
-        restartCardAnimation()
-    }
-    function animatePrevCard(){
-        props.onPrev()
-        restartCardAnimation()
-    }
+//     function restartCardAnimation(){
+//         animation.stop()
+//         animation.set(initialStyles)
+//         // animation.start(targetStyles)
+//     }
+//     function animateNextCard(){
+//         props.onNext()
+//         restartCardAnimation()
+//     }
+//     function animatePrevCard(){
+//         props.onPrev()
+//         restartCardAnimation()
+//     }
 
-    useEffect(()=>{
-        if (!frontSideVisible || isHovered){
-            animation.stop()
-        } else {
-            // animation.start(targetStyles)
-        }
-    },[frontSideVisible,isHovered,props.duration])
+//     useEffect(()=>{
+//         if (!frontSideVisible || isHovered){
+//             animation.stop()
+//         } else {
+//             // animation.start(targetStyles)
+//         }
+//     },[frontSideVisible,isHovered,props.duration])
     
-    return (
-    <StyledNodeCard id="node-card" $isHovered={isHovered}
-        onClick={handleClick}  
-        onMouseEnter={()=>{setIsHovered(true)}} 
-        onMouseLeave={()=>{setIsHovered(false)}}
-    >
-        <TimerBar $isVisible={frontSideVisible} $isHovered={isHovered}
-            animate={animation} 
-            initial={initialStyles}
-            onAnimationComplete={animateNextCard}
-        />
-        <div className="card-controls" >
-            <IconButton className="nav-btn top left outlined" 
-                onClick={animatePrevCard}
-            >
-                <KeyboardArrowLeftIcon  />
-            </IconButton>
-            <IconButton className="nav-btn top right outlined" 
-                onClick={animateNextCard}
-            >
-                <KeyboardArrowRightIcon disabled={true}/>
-            </IconButton>
-            {!frontSideVisible && <div>
-                <IconButton className="nav-btn bottom left outlined" 
-                    onClick={() => {props.onDecreaseNodeFreq(props.nodeID)}}
-                >
-                    <ArrowDropDownIcon />
-                </IconButton>
-                <IconButton className="nav-btn bottom right outlined" 
-                    onClick={() => {props.onIncreaseNodeFreq(props.nodeID)}}
-                >
-                    <ArrowDropUpIcon />
-                </IconButton>
-            </div>}
-        </div>
+//     return (
+//     <StyledNodeCard id="node-card" $isHovered={isHovered}
+//         onClick={handleClick}  
+//         onMouseEnter={()=>{setIsHovered(true)}} 
+//         onMouseLeave={()=>{setIsHovered(false)}}
+//     >
+//         <TimerBar $isVisible={frontSideVisible} $isHovered={isHovered}
+//             animate={animation} 
+//             initial={initialStyles}
+//             onAnimationComplete={animateNextCard}
+//         />
+//         <div className="card-controls" >
+//             <IconButton className="nav-btn top left outlined" 
+//                 onClick={animatePrevCard}
+//             >
+//                 <KeyboardArrowLeftIcon  />
+//             </IconButton>
+//             <IconButton className="nav-btn top right outlined" 
+//                 onClick={animateNextCard}
+//             >
+//                 <KeyboardArrowRightIcon disabled={true}/>
+//             </IconButton>
+//             {!frontSideVisible && <div>
+//                 <IconButton className="nav-btn bottom left outlined" 
+//                     onClick={() => {props.onDecreaseNodeFreq(props.nodeID)}}
+//                 >
+//                     <ArrowDropDownIcon />
+//                 </IconButton>
+//                 <IconButton className="nav-btn bottom right outlined" 
+//                     onClick={() => {props.onIncreaseNodeFreq(props.nodeID)}}
+//                 >
+//                     <ArrowDropUpIcon />
+//                 </IconButton>
+//             </div>}
+//         </div>
         
-        <div className="card-content" >
-            <StyledCardSide id="front-side" $isVisible={frontSideVisible} $isHovered={isHovered}>
-                {props.nodeData.title && <h1 >{props.nodeData.title} </h1>}
-                <p>{props.nodeData.content}</p> 
-            </StyledCardSide>
-            <StyledCardSide id="back-side" $isVisible={!frontSideVisible} $isHovered={isHovered}>
-                <h1 > Node #{props.nodeData.id+1} [{currTimelineIdx+1} / {nodeIDsTimeline.length}] </h1>
-                <p>Inspiration: {props.nodeData.inspiration}  </p><br></br>
-                <p className="frequency">
-                    {(props.nodeData.frequency * 100).toFixed(1)}% Likely to appear
-                </p>
-            </StyledCardSide>
-        </div>
+//         <div className="card-content" >
+//             <StyledCardSide id="front-side" $isVisible={frontSideVisible} $isHovered={isHovered}>
+//                 {props.nodeData?.title && <h1 >{props.nodeData?.title} </h1>}
+//                 <p>{props.nodeData?.content}</p> 
+//             </StyledCardSide>
+//             <StyledCardSide id="back-side" $isVisible={!frontSideVisible} $isHovered={isHovered}>
+//                 <h1 > Node #{props.nodeData?.id+1} [{currTimelineIdx+1} / {nodeIDsTimeline.length}] </h1>
+//                 <p>Inspiration: {props.nodeData?.inspiration}  </p><br></br>
+//                 <p className="frequency">
+//                     {(props.nodeData?.frequency * 100).toFixed(1)}% Likely to appear
+//                 </p>
+//             </StyledCardSide>
+//         </div>
         
-    </StyledNodeCard>
-    );
-}
+//     </StyledNodeCard>
+//     );
+// }
 
-const TimerBar = styled(motion.div)`
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%, 0);
-    border-radius: 2px;
-    bottom: .5px;
-    filter: ${props => props.$isVisible ? "none": (props.$isHovered ? "blur(3px)" : "blur(9px)") };
-    pointer-events: none;
-    height: 3px;
-    margin: 0 auto;
-    background-color: white;
-`
+// const TimerBar = styled(motion.div)`
+//     position: absolute;
+//     left: 50%;
+//     transform: translate(-50%, 0);
+//     border-radius: 2px;
+//     bottom: .5px;
+//     filter: ${props => props.$isVisible ? "none": (props.$isHovered ? "blur(3px)" : "blur(9px)") };
+//     pointer-events: none;
+//     height: 3px;
+//     margin: 0 auto;
+//     background-color: white;
+// `
 
-const StyledCardSide = styled.div`
-  opacity: ${props => props.$isVisible ? "1": ".15"};
-  filter: ${props => props.$isVisible ? "none": (props.$isHovered ? "blur(3px)" : "blur(9px)") };
-  transform: ${props => props.$isVisible ? "scale(1, 1)": "scale(-1, 1)"};;
-  padding: 10px 0px;
-  grid-area: 1/1;
-  pointer-events: none;
-`
+// const StyledCardSide = styled.div`
+//   opacity: ${props => props.$isVisible ? "1": ".15"};
+//   filter: ${props => props.$isVisible ? "none": (props.$isHovered ? "blur(3px)" : "blur(9px)") };
+//   transform: ${props => props.$isVisible ? "scale(1, 1)": "scale(-1, 1)"};;
+//   padding: 10px 0px;
+//   grid-area: 1/1;
+//   pointer-events: none;
+// `
 
-const StyledNodeCard = styled.div`
-  background: #00219708;
-  border-radius: 5px;
-  box-shadow: 0px 0px 4px #CCC;
-  padding: 20px 30px 30px;
-  width: 525px;
-  margin: 4px;
-  position: relative;
-  color: #EEE;
-  backdrop-filter: ${props => props.$isHovered ? "blur(7px)" : "blur(13px)"};
-  background-color: #222222C0;
-  overflow: visible;
+// const StyledNodeCard = styled.div`
+//   background: #00219708;
+//   border-radius: 5px;
+//   box-shadow: 0px 0px 4px #CCC;
+//   padding: 20px 30px 30px;
+//   width: 525px;
+//   margin: 4px;
+//   position: relative;
+//   color: #EEE;
+//   backdrop-filter: ${props => props.$isHovered ? "blur(7px)" : "blur(13px)"};
+//   background-color: #222222C0;
+//   overflow: visible;
 
-  :hover{
-    background-color: #22222230;
-  }
+//   :hover{
+//     background-color: #22222230;
+//   }
 
-  .card-controls{
-    display: none;
-  }
+//   .card-controls{
+//     display: none;
+//   }
 
-  :hover > .card-controls {
-    display: block;
-  }
+//   :hover > .card-controls {
+//     display: block;
+//   }
 
-  .outlined:hover {
-    outline: 1px solid #ffffff80;
-  }
+//   .outlined:hover {
+//     outline: 1px solid #ffffff80;
+//   }
   
-  .card-content{
-    display: grid;
-    align-items:center;
-    pointer-events: none;
-    height: auto;
-  }
+//   .card-content{
+//     display: grid;
+//     align-items:center;
+//     pointer-events: none;
+//     height: auto;
+//   }
 
-  .nav-btn {
-	color: white;
-    position: absolute;
-    z-index: 1;
-  }
+//   .nav-btn {
+// 	color: white;
+//     position: absolute;
+//     z-index: 1;
+//   }
 
-  h1 {
-    text-align: center;
-    font-size: 1.2em;
-    margin: 0px 15px 20px;
-  }
+//   h1 {
+//     text-align: center;
+//     font-size: 1.2em;
+//     margin: 0px 15px 20px;
+//   }
 
-  p {
-    margin: auto;
-    font-size: 1.2em;
-  }
+//   p {
+//     margin: auto;
+//     font-size: 1.2em;
+//   }
 
-  .frequency {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: -8px;
-    font-size: .8em;
-  }
+//   .frequency {
+//     position: absolute;
+//     left: 0;
+//     right: 0;
+//     bottom: -8px;
+//     font-size: .8em;
+//   }
   
-  .left { left: 15px; }
+//   .left { left: 15px; }
 
-  .right { right: 15px; }
+//   .right { right: 15px; }
 
-  .top { top: 20px; }
+//   .top { top: 20px; }
 
-  .bottom { bottom: 15px; }
+//   .bottom { bottom: 15px; }
 
-`;
+// `;
--- public/components/NodeCardsArea.js
+++ public/components/NodeCardsArea.js
@@ -1,100 +1,100 @@
-import React, {useState, useEffect} from "react";
-import styled from "styled-components";
-import NodeCard from "./NodeCard.js";
-import { useNodesStore, useNodesTimelineStore } from "../Store.js";
+// import React, {useState, useEffect} from "react";
+// import styled from "styled-components";
+// import NodeCard from "./NodeCard.js";
+// import { useNodesStore, useNodesTimelineStore } from "../Store.js";
 
-export default function NodeCardsArea() {
-	let nodeIDsTimeline = useNodesTimelineStore(state => state.nodeIDsTimeline)
-	let addNodeIDToTimeline = useNodesTimelineStore(state => state.addNodeIDToTimeline)
-    let currTimelineIdx = useNodesTimelineStore(state => state.currTimelineIdx)
-	let setCurrTimelineIdx = useNodesTimelineStore(state => state.setCurrTimelineIdx)
-	let nodes = useNodesStore(state => state.nodes)
-	let setNodes = useNodesStore(state => state.setNodes)
-	let currNodeID = useNodesTimelineStore(state => state.currNodeID)
-	let currCardDuration = nodes[currNodeID].charCount * .25
+// export default function NodeCardsArea() {
+// 	let nodeIDsTimeline = useNodesTimelineStore(state => state.nodeIDsTimeline)
+// 	let addNodeIDToTimeline = useNodesTimelineStore(state => state.addNodeIDToTimeline)
+//     let currTimelineIdx = useNodesTimelineStore(state => state.currTimelineIdx)
+// 	let setCurrTimelineIdx = useNodesTimelineStore(state => state.setCurrTimelineIdx)
+// 	let nodes = useNodesStore(state => state.nodes)
+// 	let setNodes = useNodesStore(state => state.setNodes)
+// 	let currNodeID = useNodesTimelineStore(state => state.currNodeID)
+// 	let currCardDuration = nodes[currNodeID]?.charCount * .25
 
-	if (nodeIDsTimeline.length === 0){
-		addNodeIDToTimeline(getWeightedRandomNodeID())
-	}
+// 	if (nodeIDsTimeline.length === 0){
+// 		addNodeIDToTimeline(getWeightedRandomNodeID())
+// 	}
 
-	function onNextNodeCard(){
-		let isAtEndOfTimeline = currTimelineIdx === nodeIDsTimeline.length - 1
-		if(isAtEndOfTimeline) {
-			let newNodeID = getWeightedRandomNodeID()
-			while(newNodeID === nodeIDsTimeline.at(-1)){
-				newNodeID = getWeightedRandomNodeID()
-			}
-			addNodeIDToTimeline(newNodeID)
-			return newNodeID
-		} else {
-			setCurrTimelineIdx(currTimelineIdx+1)
-			return nodeIDsTimeline[currTimelineIdx+1]
-		}
-	}
+// 	function onNextNodeCard(){
+// 		let isAtEndOfTimeline = currTimelineIdx === nodeIDsTimeline.length - 1
+// 		if(isAtEndOfTimeline) {
+// 			let newNodeID = getWeightedRandomNodeID()
+// 			while(newNodeID === nodeIDsTimeline.at(-1)){
+// 				newNodeID = getWeightedRandomNodeID()
+// 			}
+// 			addNodeIDToTimeline(newNodeID)
+// 			return newNodeID
+// 		} else {
+// 			setCurrTimelineIdx(currTimelineIdx+1)
+// 			return nodeIDsTimeline[currTimelineIdx+1]
+// 		}
+// 	}
 
-	function onPrevNodeCard(){
-		let isAtBeginningOfTimeline = currTimelineIdx === 0
-		if(!isAtBeginningOfTimeline) {
-			setCurrTimelineIdx(currTimelineIdx-1)
-			return nodeIDsTimeline[currTimelineIdx+1]
-		}
-	}
+// 	function onPrevNodeCard(){
+// 		let isAtBeginningOfTimeline = currTimelineIdx === 0
+// 		if(!isAtBeginningOfTimeline) {
+// 			setCurrTimelineIdx(currTimelineIdx-1)
+// 			return nodeIDsTimeline[currTimelineIdx+1]
+// 		}
+// 	}
 
-	function getWeightedRandomNodeID() {
-		let randNum = Math.random(); // range of [0,1)
-		let counter = 0;
-		for (let i = 0; i < nodes.length; i++) {
-			let currNodeFreq = nodes[i].frequency
-			//likelyhood of randNum being inside the range is === to the nodes appearance frequency
-			let isRandNumInNodeRange = randNum >= counter && randNum < (counter + currNodeFreq)
-			if (isRandNumInNodeRange) {
-				return nodes[i].id
-			} else {
-				counter += currNodeFreq
-			}
-		}
-	}
+// 	function getWeightedRandomNodeID() {
+// 		let randNum = Math.random(); // range of [0,1)
+// 		let counter = 0;
+// 		for (let i = 0; i < nodes.length; i++) {
+// 			let currNodeFreq = nodes[i].frequency
+// 			//likelyhood of randNum being inside the range is === to the nodes appearance frequency
+// 			let isRandNumInNodeRange = randNum >= counter && randNum < (counter + currNodeFreq)
+// 			if (isRandNumInNodeRange) {
+// 				return nodes[i].id
+// 			} else {
+// 				counter += currNodeFreq
+// 			}
+// 		}
+// 	}
 
-	function changeNodeFrquency(nodeID, isIncreased) {
-		let numNodes = nodes.length;
-		let numerator = isIncreased ? 1 : -1;
-		let freqModifier = numerator / (numNodes * numNodes);
+// 	function changeNodeFrquency(nodeID, isIncreased) {
+// 		let numNodes = nodes.length;
+// 		let numerator = isIncreased ? 1 : -1;
+// 		let freqModifier = numerator / (numNodes * numNodes);
 
-		let newFrequency = nodes[nodeID].frequency + numNodes * freqModifier;
+// 		let newFrequency = nodes[nodeID].frequency + numNodes * freqModifier;
 
-		let tempNodes = [...nodes]
-		if (Math.abs(1 - newFrequency) >= 1e-12) {
-			tempNodes[nodeID].frequency = newFrequency;
+// 		let tempNodes = [...nodes]
+// 		if (Math.abs(1 - newFrequency) >= 1e-12) {
+// 			tempNodes[nodeID].frequency = newFrequency;
 
-			tempNodes.forEach((node) => {
-				node.frequency -= freqModifier;
-			});
-		}
-		setNodes(tempNodes)
-	}
+// 			tempNodes.forEach((node) => {
+// 				node.frequency -= freqModifier;
+// 			});
+// 		}
+// 		setNodes(tempNodes)
+// 	}
 
-	let increaseNodeFreq = (nodeID) => changeNodeFrquency(nodeID, true)
-	let decreaseNodeFreq = (nodeID) => changeNodeFrquency(nodeID, false)
-	return (
-		<StyledNodeCardsArea id="node-cards-area">
-			<NodeCard 
-				onNext={onNextNodeCard} 
-				onPrev={onPrevNodeCard} 
-				onIncreaseNodeFreq={increaseNodeFreq} 
-				onDecreaseNodeFreq={decreaseNodeFreq} 
-				nodeData = {nodes[currNodeID]}
-				duration = {currCardDuration}
-			/>
-		</StyledNodeCardsArea>
-	);
-}
+// 	let increaseNodeFreq = (nodeID) => changeNodeFrquency(nodeID, true)
+// 	let decreaseNodeFreq = (nodeID) => changeNodeFrquency(nodeID, false)
+// 	return (
+// 		<StyledNodeCardsArea id="node-cards-area">
+// 			<NodeCard 
+// 				onNext={onNextNodeCard} 
+// 				onPrev={onPrevNodeCard} 
+// 				onIncreaseNodeFreq={increaseNodeFreq} 
+// 				onDecreaseNodeFreq={decreaseNodeFreq} 
+// 				nodeData = {nodes[currNodeID]}
+// 				duration = {currCardDuration}
+// 			/>
+// 		</StyledNodeCardsArea>
+// 	);
+// }
 
-let StyledNodeCardsArea = styled.div`
-	position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -70%);
-	margin: 20px;
-	overflow-y: hidden;
-	width: 600px;
-`;
+// let StyledNodeCardsArea = styled.div`
+// 	position: absolute;
+//     left: 50%;
+//     top: 50%;
+//     transform: translate(-50%, -70%);
+// 	margin: 20px;
+// 	overflow-y: hidden;
+// 	width: 600px;
+// `;
