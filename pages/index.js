8a619ddddbba64d82b9dbea6097323e9ca6a666d
Alex C <andres.al.campos@gmail.com>
2023-02-03 00:16:12 -0800

    readding node cards

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home({ nodesString }) {
 			</div>
 			<div>
 				{/* <IdeaCompositionArea onAdd={addNode} /> */}
-				{/* <NodeCardsArea /> */}
+				<NodeCardsArea />
 			</div>
 		</StyledHome>
 	);
