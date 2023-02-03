01e4afb92c5f09d13f0da008e7df681a1b16ab54
Alex C <andres.al.campos@gmail.com>
2023-02-03 00:16:27 -0800

    trying to fix hidration errors, wip

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
-
-// module.exports = nextConfig
-module.exports = {
   env: {
     IDEARIUM_URI: process.env.IDEARIUM_URI,
     TEST_URI: process.env.TEST_URI,
-  }
+  },
+  compiler: {
+    styledComponents: true,
+  },
 }
+
+module.exports = nextConfig
