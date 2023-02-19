8686edc98049b30a0f70e632f6056c1cc58d71ad
Alex C <andres.al.campos@gmail.com>
2023-02-18 20:33:51 -0800

    adding home page imports, adding jotai

--- app/page.js
+++ app/page.js
@@ -1,57 +1,78 @@
 import styles from "./page.module.css";
+
+import styled from "styled-components";
+import NodeCardsArea from '../public/components/NodeCardsArea.js';
+import IdeaCompositionArea from '../public/components/IdeaCompositionArea.js';
+import mongoose from 'mongoose';
+import {Node} from '../models/Node.js'
+import {useNodesStore} from '../public/Store';
+import { atom, useAtom } from 'jotai'
+
 export default async function Home() {
-  // const [nodes, setNodes] = useNodesStore(state => [state.nodes, state.setNodes])
-  // console.log("starting nodes:", nodes)
-  // let testNodes = await getNodesFromDB()
-  // console.log("stestNodes:", testNodes)
+	// const [nodes, setNodes] = useNodesStore(state => [state.nodes, state.setNodes])  //causes useref is not a funcion error
+	
+	async function getNodesFromDB(){
+		await mongoose.connect(process.env.IDEARIUM_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		// const data = await db.collection('asdfg').toArray();
+		let nodes = await Node.find({}).exec()
+		let nodesJSON = JSON.parse(JSON.stringify(nodes))
+		await mongoose.connection.close();
+		return nodesJSON
+	}
+	
+	let testNodes = await getNodesFromDB()
+	console.log("stestNodes:", testNodes)
 
-  // useEffect(() => {
-  // 	let nodesJSON = JSON.parse(nodesString)
-  // 	console.log("setting the nodes:", nodesJSON)
-  // 	setNodes(nodesJSON)
-  // }, [nodesString])
+	// useEffect(() => {
+	// 	let nodesJSON = JSON.parse(nodesString)
+	// 	console.log("setting the nodes:", nodesJSON)
+	// 	setNodes(nodesJSON)
+	// }, [nodesString])
 
-  // setWordCounts()
+	// setWordCounts()
 
-  // function getNodeCharCount(node) {
-  // 	//let titleCount = node.title.split(" ").filter(word => word !== "").length
-  // 	let titleCount = node.title.length
-  // 	let contentCount = node.content.length
-  // 	return titleCount + contentCount
-  // }
+	// function getNodeCharCount(node) {
+	// 	//let titleCount = node.title.split(" ").filter(word => word !== "").length
+	// 	let titleCount = node.title.length
+	// 	let contentCount = node.content.length
+	// 	return titleCount + contentCount
+	// }
 
-  // function addNode(newNodeData){
-  // 	let updatedNodes = updateNodeFrequencies(nodes)
-  // 	let newNode = {
-  // 		id: nodes.length,
-  // 		title: newNodeData.title,
-  // 		content: newNodeData.content,
-  // 		inspiration: newNodeData.inspiration,
-  // 		frequency: 1 / (nodes.length+1),
-  // 		charCount: getNodeCharCount(newNodeData)
-  // 	}
-  // 	let newNodes = [...updatedNodes, newNode]
-  // 	setNodes(newNodes)
-  // }
+	// function addNode(newNodeData){
+	// 	let updatedNodes = updateNodeFrequencies(nodes)
+	// 	let newNode = {
+	// 		id: nodes.length,
+	// 		title: newNodeData.title,
+	// 		content: newNodeData.content,
+	// 		inspiration: newNodeData.inspiration,
+	// 		frequency: 1 / (nodes.length+1),
+	// 		charCount: getNodeCharCount(newNodeData)
+	// 	}
+	// 	let newNodes = [...updatedNodes, newNode]
+	// 	setNodes(newNodes)
+	// }
 
-  // function updateNodeFrequencies(nodes) {
-  // 	let newFreqScalar = nodes.length / (nodes.length + 1)
-  //     nodes.forEach((node) => {
-  // 		node.frequency = node.frequency * newFreqScalar
-  //     });
-  // 	return nodes
-  // }
+	// function updateNodeFrequencies(nodes) {
+	// 	let newFreqScalar = nodes.length / (nodes.length + 1)
+	//     nodes.forEach((node) => {
+	// 		node.frequency = node.frequency * newFreqScalar
+	//     });
+	// 	return nodes
+	// }
 
-  // function setWordCounts(){
-  // 	nodes.forEach(node => {
-  // 		node.charCount = getNodeCharCount(node)
-  // 	});
-  // }
-  return (
-	<main class={styles.main}>
-		<div id="Home">
-        	<div>Hello World</div>
-		</div>
-    </main>
-  );
+	// function setWordCounts(){
+	// 	nodes.forEach(node => {
+	// 		node.charCount = getNodeCharCount(node)
+	// 	});
+	// }
+	return (
+		<main className={styles.main}>
+			<div id="Home">
+				<div>Hello World</div>
+			</div>
+		</main>
+	);
 }
