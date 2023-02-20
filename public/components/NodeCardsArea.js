d79d85cfad8406531db413150d0fa315a9d40d3e
Alex C <andres.al.campos@gmail.com>
2023-02-19 17:27:43 -0800

    adding styled componetns. seems to work on client componetns.

--- public/components/NodeCardsArea.js
+++ public/components/NodeCardsArea.js
@@ -1,28 +1,28 @@
 'use client';
 // import React, {useState, useEffect} from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 import NodeCard from "./NodeCard.js";
-// import { useNodesStore, useNodesTimelineStore } from "../Store.js";
 import { atom, useAtom } from 'jotai';
 import { useNodesTimelineStore } from './../Store';
 
 export const nodesAtom = atom([])
 export const currNodeIDAtom = atom('')
 export const getNodesListAtom = atom( (get) => get(nodesAtom).map((node) => <li key={node._id}> {node._id} {node.title} { node.content} </li>) )
-export const nodeIDsTimeline = atom([])
-export const currTimelineIdx = atom('')
+export const nodeIDsTimelineAtom = atom([])
+export const currTimelineIdxAtom = atom('')
 
 export default function NodeCardsArea({nodesData}) {
     const [nodes, setNodes] = useAtom(nodesAtom)
     const [currNodeID, setCurrNodeID] = useAtom(currNodeIDAtom)
+    const [nodeIDsTimeline, setNodeIDsTimeline] = useAtom(nodeIDsTimelineAtom)
+    const [currTimelineIdx, setCurrTimelineIdx] = useAtom(currTimelineIdxAtom)
 
 	setNodes(nodesData)
     const [nodesList] = useAtom(getNodesListAtom)
 
-    // let returnedNodes = printNodes();
-
 	// if (nodeIDsTimeline.length === 0){
-	// 	addNodeIDToTimeline(getWeightedRandomNodeID())
+	// 	setNodeIDsTimeline((prev) => [...prev, getWeightedRandomNodeID()] )
+	// 	// addNodeIDToTimeline()
 	// }
 
 	// function onNextNodeCard(){
@@ -48,20 +48,20 @@ export default function NodeCardsArea({nodesData}) {
 	// 	}
 	// }
 
-	// function getWeightedRandomNodeID() {
-	// 	let randNum = Math.random(); // range of [0,1)
-	// 	let counter = 0;
-	// 	for (let i = 0; i < nodes.length; i++) {
-	// 		let currNodeFreq = nodes[i].frequency
-	// 		//likelyhood of randNum being inside the range is === to the nodes appearance frequency
-	// 		let isRandNumInNodeRange = randNum >= counter && randNum < (counter + currNodeFreq)
-	// 		if (isRandNumInNodeRange) {
-	// 			return nodes[i].id
-	// 		} else {
-	// 			counter += currNodeFreq
-	// 		}
-	// 	}
-	// }
+	function getWeightedRandomNodeID() {
+		let randNum = Math.random(); // range of [0,1)
+		let counter = 0;
+		for (let i = 0; i < nodes.length; i++) {
+			let currNodeFreq = nodes[i].frequency
+			//likelyhood of randNum being inside the range is === to the nodes appearance frequency
+			let isRandNumInNodeRange = randNum >= counter && randNum < (counter + currNodeFreq)
+			if (isRandNumInNodeRange) {
+				return nodes[i].id
+			} else {
+				counter += currNodeFreq
+			}
+		}
+	}
 
 	// function changeNodeFrquency(nodeID, isIncreased) {
 	// 	let numNodes = nodes.length;
@@ -85,9 +85,9 @@ export default function NodeCardsArea({nodesData}) {
 	// let decreaseNodeFreq = (nodeID) => changeNodeFrquency(nodeID, false)
 	return (
         
-		<div id="node-cards-area">
+		<StyledNodeCardsArea id="node-cards-area">
             <div> Node Cards Area </div>
-            
+			<ul>{ nodesList }</ul>
 		 	{/* <NodeCard 
 		 		// onNext={onNextNodeCard} 
 		 		// onPrev={onPrevNodeCard} 
@@ -97,16 +97,16 @@ export default function NodeCardsArea({nodesData}) {
 		 		duration = {1000}
 		 		// duration = {currCardDuration}
 		 	/> */}
-		</div>
+		</StyledNodeCardsArea>
 	);
 }
 
-// let StyledNodeCardsArea = styled.div`
-// 	position: absolute;
-//     left: 50%;
-//     top: 50%;
-//     transform: translate(-50%, -70%);
-// 	margin: 20px;
-// 	overflow-y: hidden;
-// 	width: 600px;
-// `;
+let StyledNodeCardsArea = styled.div`
+	position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -70%);
+	margin: 20px;
+	overflow-y: hidden;
+	width: 600px;
+`;
