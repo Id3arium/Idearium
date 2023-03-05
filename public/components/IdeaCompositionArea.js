74d50c3173b81cc4ef0bfebc3d9e30021e467c03
Alex C <andres.al.campos@gmail.com>
2023-03-04 20:16:46 -0800

    redoing ideacompositionarea with atoms

--- public/components/IdeaCompositionArea.js
+++ public/components/IdeaCompositionArea.js
@@ -4,126 +4,134 @@ import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
 import _ from "lodash";
+import { v4 as uuidv4 } from 'uuid';
 import styled from "@emotion/styled";
+import { atom, useAtom, useSetAtom, useAtomValue } from 'jotai';
+import { nodesAtom, addNewNodeAtom } from '@/public/atoms.js';
 
-function IdeaCompositionArea(props) {
-  const [note, setNote] = useState({ title: "", content: "", inspiration: "" });
-  const [isExpanded, setIsExpanded] = useState(false);
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setNote((prevNote) => {
-      return {
-        ...prevNote,
-        [name]: value
-      };
-    });
-  }
-  function submitNote(e) {
-    e.preventDefault();
-    let emptyNote = { title: "", content: "", inspiration: "" };
-    if (!_.isEqual(note, emptyNote)) {
-      props.onAdd(note);
-      // let newNode = {
-      //  _id: uuidv4(),
-      // 	id: nodes.length,
-      // 	title: newNodeData.title,
-      // 	content: newNodeData.content,
-      // 	inspiration: newNodeData.inspiration,
-      // 	frequency: 1 / (nodes.length + 1),
-      // 	// charCount: getNodeCharCount(newNodeData)
-      // }
-      setNote(emptyNote);
+const noteAtom = atom({ title: "", content: "", inspiration: "" })
+const isExpandedAtom = atom(false)
+
+function IdeaCompositionArea() {
+    const [note, setNote] = useAtom(noteAtom);
+    const [isExpanded, setIsExpanded] = useAtom(isExpandedAtom);
+    const addNewNode = useSetAtom(addNewNodeAtom);
+    const nodes = useAtomValue(nodesAtom);
+    
+    function onInputChanged(e) {
+        const { name, value } = e.target;
+        console.log("onInputChanged", note)
+        setNote((prevNote) => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
     }
-  }
-
-  return (
-    <StyledCreateArea 
-        id="create-area"
-        onFocus={()=>setIsExpanded(true)}
-        onBlur={()=>setIsExpanded(false)}
-    >
-        <form className="create-note">
-            {isExpanded && 
-                <input
-                    name="title"
-                    placeholder="Title"
-                    value={note.title}
-                    onChange={handleChange}
-                />
+    function onAddButtonClicked(e) {
+        e.preventDefault();
+        let emptyNote = { title: "", content: "", inspiration: "" };
+        console.log("e",e)
+        if (!_.isEqual(note, emptyNote)) {
+            let newNode = {
+                _id: uuidv4(),
+            	id: nodes.length,
+            	title: note.title,
+            	content: note.content,
+            	inspiration: note.inspiration,
+            	frequency: 1 / (nodes.length + 1),
             }
+            addNewNode(newNode)
+            setNote(emptyNote)
+        }
+    }
 
-            <textarea
-                name="content"
-                placeholder={isExpanded ? "Content" : "Compose an Idea..." }
-                rows={isExpanded ? "4" : "1"}
-                value={note.content}
-                onChange={handleChange}
-            />
-            {isExpanded &&
-                <input
-                    name="inspiration"
-                    placeholder={"Inspiration" }
-                    rows={1}
-                    value={note.inspiration}
-                    onChange={handleChange}
+    return (
+        <StyledCreateArea 
+            id="create-area"
+            onFocus={()=>setIsExpanded(true)}
+            onBlur={()=>setIsExpanded(false)}
+        >
+            <form className="create-note">
+                {isExpanded && 
+                    <input
+                        name="title"
+                        placeholder="Title"
+                        value={note.title}
+                        onChange={(e)=>{onInputChanged(e)}}
+                    />
+                }
+                <textarea
+                    name="content"
+                    placeholder={isExpanded ? "Content" : "Compose an Idea..." }
+                    rows={isExpanded ? "4" : "1"}
+                    value={note.content}
+                    onChange={(e)=>{onInputChanged(e)}}
                 />
-            }
-            <Zoom in={isExpanded}>
-            <Fab onClick={submitNote}>
-                <AddIcon />
-            </Fab>
-            </Zoom>
-        </form>
-    </StyledCreateArea>
-  );
+                {isExpanded &&
+                    <input
+                        name="inspiration"
+                        placeholder={"Inspiration" }
+                        rows={1}
+                        value={note.inspiration}
+                        onChange={(e)=>{onInputChanged(e)}}
+                    />
+                }
+                <Zoom in={isExpanded}>
+                <Fab onClick={(e)=>{onAddButtonClicked(e)}}>
+                    <AddIcon />
+                </Fab>
+                </Zoom>
+            </form>
+        </StyledCreateArea>
+    );
 }
 
 let StyledCreateArea = styled.div`
-  width: 400px;
-  height: 300px;
-  margin: 30px 20px;
-  position: absolute;
-  left: 0;
-
-  form.create-note {
-    position: relative;
-    backdrop-filter: blur(7px);
-    background: #00000000;
-    padding: 10px;
-    border-radius: 7px;
-    box-shadow: 0px 0px 4px #CCC;
-  }
-
-  form.create-note input,
-  form.create-note textarea {
-    color: #EEE;
-    width: 100%;
-    height: 30px;
-    border: none;
-    background: #00000000;
-    padding: 4px;
-    outline: none;
-    font-size: 1.2em;
-    font-family: inherit;
-    resize: none;
-  }
-
-  form.create-note button {
+    width: 400px;
+    height: 300px;
+    margin: 30px 20px;
     position: absolute;
-    right: 12px;
-    bottom: -18px;
-    background: #1313f580;
-    backdrop-filter: blur(5px);
-    color: #CCC;
-    border: 1px solid black;
-    border-radius: 50%;
-    width: 36px;
-    height: 36px;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
-    cursor: pointer;
-    outline: none;
-  }
+    left: 0;
 
+    form.create-note {
+        position: relative;
+        backdrop-filter: blur(7px);
+        background: #00000000;
+        padding: 10px;
+        border-radius: 7px;
+        box-shadow: 0px 0px 4px #CCC;
+    }
+
+    form.create-note input,
+    form.create-note textarea {
+        color: #EEE;
+        width: 100%;
+        height: 30px;
+        border: none;
+        background: #00000000;
+        padding: 4px;
+        outline: none;
+        font-size: 1.2em;
+        font-family: inherit;
+        resize: none;
+    }
+
+    form.create-note button {
+        position: absolute;
+        right: 12px;
+        bottom: -18px;
+        background: #1313f580;
+        backdrop-filter: blur(5px);
+        color: #CCC;
+        border: 1px solid black;
+        border-radius: 50%;
+        width: 36px;
+        height: 36px;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.3);
+        cursor: pointer;
+        outline: none;
+    }
 `
 
 export default IdeaCompositionArea;
