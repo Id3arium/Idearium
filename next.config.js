099cefe2e873eb49a4d869c672568d98a07a0569
Alex C <andres.al.campos@gmail.com>
2023-03-26 20:55:05 -0700

    guess i dont need any of this either

--- next.config.js
+++ next.config.js
@@ -5,12 +5,4 @@ module.exports = {
   experimental: {
     appDir: true,
   },
-  env: {
-    IDEARIUM_URI: process.env.IDEARIUM_URI,
-    DATABASE_URL: process.env.DATABASE_URL,
-    TEST_URI: process.env.TEST_URI,
-  },
-  compiler: {
-    styledComponents: true,
-  },
 }
