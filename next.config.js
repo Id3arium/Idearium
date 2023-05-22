5720a35a0012a83ba6af6311d7b57891812d7a47
Alex C <andres.al.campos@gmail.com>
2023-05-21 19:48:06 -0700

    undoing transpiling. library is deprecated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['three-render-objects']);
+// const withTM = require('next-transpile-modules')(['three-render-objects']);
 
-module.exports = withTM({
+// module.exports = withTM({
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true,
+//   },
+// });
+module.exports = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-});
+};
