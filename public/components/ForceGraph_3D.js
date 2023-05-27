916c2f221ab8681401f714e1d5e4c41801934cc0
Alex C <andres.al.campos@gmail.com>
2023-05-26 21:48:07 -0700

    making the graph rotae constantly! messes up the other animations tho.

--- public/components/ForceGraph_3D.js
+++ public/components/ForceGraph_3D.js
@@ -1,11 +1,11 @@
 'use client';
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useRef, useEffect } from "react";
 import ForceGraph3D, { ForceGraphMethods } from "react-force-graph-3d";
 import styled from 'styled-components'
 
 export default function ForceGraph_3D() {
-    let ref = useRef();
-
+    let graphRef = useRef();
+  
     const N = 300;
     const data = {
       nodes: [...Array(N).keys()].map((i) => ({ id: i })),
@@ -17,41 +17,61 @@ export default function ForceGraph_3D() {
         })),
     };
     const handleClick = useCallback(
-        (node) => {
-          const distance = 40;
-          const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
-          if (ref.current) {
-            console.log(ref.current);
-            ref.current.cameraPosition(
-              {
-                x: node.x * distRatio,
-                y: node.y * distRatio,
-                z: node.z * distRatio
-              },
-              node,
-              3000
-            );
-          }
-        },
-        [ref]
-      );
+      (node) => {
+        const distance = 40;
+        const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+        if (graphRef.current) {
+          graphRef.current.cameraPosition(
+            {
+              x: node.x * distRatio,
+              y: node.y * distRatio,
+              z: node.z * distRatio,
+            },
+            node,
+            3000
+          );
+        }
+      },
+      [graphRef]
+    );
+  
+    useEffect(() => {
+        let distanceFromCenter = 1500
+        graphRef.current.cameraPosition({ z: distanceFromCenter });
     
-      return (
-        <DivForceGraph3D>
-          <ForceGraph3D
-            ref={ref}
-            graphData={data}
-            nodeLabel="id"
-            nodeAutoColorBy="group"
-            onNodeClick={handleClick}
-          />
-        </DivForceGraph3D>
-      );
-}
-
-let DivForceGraph3D = styled.div `
-    border: 2px solid #888;
-    margin: 0px auto;
-    width: 1040px;
-    height: 300px;
-`
+        // Camera orbit
+        let angle = 0;
+        const rotationSpeed = 0.001; 
+        const rotationInterval = setInterval(() => {
+            graphRef.current.cameraPosition({
+            x: distanceFromCenter * Math.sin(angle),
+            z: distanceFromCenter * Math.cos(angle),
+          });
+          angle -= rotationSpeed;
+        }, 10);
+    
+        return () => {
+          clearInterval(rotationInterval); // Clear the interval when the component is unmounted
+        };
+      }, []);
+  
+    return (
+      <DivForceGraph3D>
+        <ForceGraph3D
+          ref={graphRef}
+          graphData={data}
+          nodeLabel="id"
+          nodeAutoColorBy="group"
+          onNodeClick={handleClick}
+        />
+      </DivForceGraph3D>
+    );
+  }
+  
+  let DivForceGraph3D = styled.div`
+    // border: 2px solid #888;
+    // background-color: blue;
+    // margin: 0px auto;
+    // width: 1040px;
+    // height: 100hv;
+  `;
