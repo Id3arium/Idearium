b59fe1a22c85175c46e36f5707054f94176ff8b1
Alex C <andres.al.campos@gmail.com>
2023-07-01 21:51:35 -0700

    bughunting in deployment build

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
 
 import styles from "./page.module.css";
 import React from 'react'
-import NodeCardsArea from '@/public/components/NodeCardsArea.js';
+// import NodeCardsArea from '@/public/components/NodeCardsArea.js';
 // import NodeTimeline from '@/public/components/NodeTimeline.js';
-import IdeaCompositionArea from '@/public/components/IdeaCompositionArea.js';
+// import IdeaCompositionArea from '@/public/components/IdeaCompositionArea.js';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
 			<div id="Home">
-				<IdeaCompositionArea />
-				<NodeCardsArea 
-					// nodes={nodesFromServer}
-				/>
+				{/* <IdeaCompositionArea /> */}
+				{/* <NodeCardsArea 
+					nodes={nodesFromServer}
+				/> */}
 				{/* <NodeTimeline /> */}
 			</div>
 		</main>
