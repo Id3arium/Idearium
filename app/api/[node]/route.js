1c1d1ce29e58b441a0123908ecce175bbd7b82ac
Alex C <andres.al.campos@gmail.com>
2023-07-04 22:44:20 -0700

    some cleanup

--- app/api/[node]/route.js
+++ app/api/[node]/route.js
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server" 
+import { NextResponse } from "next/server"
 import { useSearchParams } from 'next/navigation';
 import { parse } from 'url';
 import qs from 'qs';
@@ -27,87 +27,90 @@ import { getNodes, createNode, deleteNode, getNextRandomNode, getNodeByID } from
 // }
 
 export async function GET(request) {
-    const searchParams = request.nextUrl.searchParams
-    
-    const hasNextRandomNodeParam = searchParams.get('next-random-node') === 'true';
-    const currNodeId = searchParams.get('curr-node-id');
+   const searchParams = request.nextUrl.searchParams
 
-    const hasGetNodeByIdParam = searchParams.get('get-node-by-id') === 'true';
-    const nodeId = searchParams.get('node-id');
-  
-    try {
+   const hasNextRandomNodeParam = searchParams.get('next-random-node') === 'true';
+   const currNodeId = searchParams.get('curr-node-id');
+
+   const hasGetNodeByIdParam = searchParams.get('get-node-by-id') === 'true';
+   const nodeId = searchParams.get('node-id');
+
+   try {
       if (hasNextRandomNodeParam) {
-        return await doGetNextRandomNode(currNodeId);
-      } else if (hasGetNodeByIdParam){
-        return await doGetNodeByID(nodeId);
+         return await doGetNextRandomNode(currNodeId);
+      } else if (hasGetNodeByIdParam) {
+         return await doGetNodeByID(nodeId);
       } else {
-        return await doGetNodes();
+         return await doGetNodes();
       }
-    } catch (error) {
+   } catch (error) {
       return NextResponse.json({ error: error.message });
-    }
+   }
 
-    async function doGetNodes() {
+   async function doGetNodes() {
       const { nodes, error } = await getNodes();
       if (error) {
-        throw new Error(error);
+         throw new Error(error);
       }
       return NextResponse.json({ nodes });
-    }
-    async function doGetNodeByID(nodeId) {
+   }
+
+   async function doGetNodeByID(nodeId) {
       const { node, error } = await getNodeByID(nodeId);
       if (error) {
-        throw new Error(error);
+         throw new Error(error);
       }
       return NextResponse.json({ node });
-    }
-    async function doGetNextRandomNode(currNodeId) {
+   }
+
+   async function doGetNextRandomNode(currNodeId) {
       const { node, error } = await getNextRandomNode(currNodeId);
       if (error) {
-        throw new Error(error);
+         throw new Error(error);
       }
-      console.log('GET getNextRandomNode node:', node);
       return NextResponse.json({ node });
-    }
+   }
 }
 
 export async function POST(request) {
-    const data = await request.json()
-    try {
-        const { node, error } = await createNode(data)
-        if (error) {
-            throw new Error(error)
-        }
-        return NextResponse.json({node})
-    } catch (error) {
-        return NextResponse.json({error: error.message})
-    }
+   const data = await request.json()
+   try {
+      const { node, error } = await createNode(data)
+      if (error) {
+         throw new Error(error)
+      }
+      return NextResponse.json({ node })
+   } catch (error) {
+      return NextResponse.json({ error: error.message })
+   }
 }
 
 export async function PUT(request, { params }) {
-    const data = await request.json()
-    console.log("PUT updating node with id", data)
-    const { node, error } = await updateNode(data)
-    try {
-        if (error) {
-            throw new Error(error)
-        }
-        return NextResponse.json({node})
-    } catch (error) {
-        return NextResponse.json({error: error.message})
-    }
+   const data = await request.json()
+   console.log("PUT updating node with id", data)
+   const { node, error } = await updateNode(data)
+   try {
+      if (error) {
+         throw new Error(error)
+      }
+      return NextResponse.json({ node })
+   } catch (error) {
+      return NextResponse.json({ error: error.message })
+   }
 }
 
 export async function DELETE(request, { params }) {
-    const data = await request.text()
-    console.log("DELETE removing node with", typeof data, " id", data)
-    try {
-        const { node, error } = await deleteNode(data)
-        if (error) {
-            throw new Error(error)
-        }
-        return NextResponse.json({node})
-    } catch (error) {
-        return NextResponse.json({error: error.message})
-    }
+   const searchParams = request.nextUrl.searchParams
+   const nodeID = searchParams.get('node-id');
+   // const data = await request.json() 
+   console.log("DELETE removing node with id", nodeID)
+   try {
+      const { node, error } = await deleteNode(nodeID)
+      if (error) {
+         throw new Error(error)
+      }
+      return NextResponse.json({ node })
+   } catch (error) {
+      return NextResponse.json({ error: error.message })
+   }
 }
