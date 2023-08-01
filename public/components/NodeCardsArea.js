3b37ded80da7806c3fe0efec7a5967e30f5657db
Alex C <andres.al.campos@gmail.com>
2023-08-01 00:44:29 -0700

    storing nodes from db in atom.

--- public/components/NodeCardsArea.js
+++ public/components/NodeCardsArea.js
@@ -2,17 +2,26 @@
 import styled from "styled-components";
 import NodeCard from "./NodeCard.js";
 import _ from "lodash";
+import { useEffect } from "react";
+import * as API from '@/lib/utils/api.js'
+import { nodesAtom } from "@/lib/utils/atoms.js";
+import { useAtom, useSetAtom } from "jotai";
 
 export default function NodeCardsArea() {
     // useHydrateAtoms([[nodesAtom, nodesFromServer.nodes]]);
 
     // const currentNode = useAtomValue(currentNodeAtom);
+    const [nodes, setNodes] = useAtom(nodesAtom)
 
-    // useEffect(() => {
-    //     if (currentNode == null) {
-    //     }
-    // }, []);
+    async function fetchNodes() {
+        const fetchedNodes = await API.fetchNodes()
+        setNodes([...fetchedNodes])
+    }
 
+    useEffect(() => {
+        fetchNodes()
+    }, []);
+    
     return (
         <StyledNodeCardsArea id="node-cards-area">
             <NodeCard />
