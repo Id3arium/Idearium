5984d6a78d091944dd5cac669a98f5ea199f57a5
Alex C <andres.al.campos@gmail.com>
2023-08-05 16:15:31 -0700

    making node and note interfaces

---
+++ lib/interfaces/Node.ts
@@ -0,0 +1,11 @@
+export interface Node {
+    id: string;
+    idx: number;
+    created: Date;
+    lastModified: Date;
+    title: string;
+    content: string;
+    inspiration: string;
+    frequency: number;
+    ranking: number;
+}
