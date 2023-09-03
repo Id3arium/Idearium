82e8994a10dbe3a9a686d299222548c845cfbd65
Alex C <andres.al.campos@gmail.com>
2023-09-02 19:51:08 -0700

    using reacthotkeys to manage hotkey handles in one place.

--- public/components/NodeCard.js
+++ public/components/NodeCard.js
@@ -6,13 +6,16 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { IconButton } from "@mui/material";
 import styled from "styled-components";
-import { motion, useAnimationControls, useAnimation } from "framer-motion";
+import { animate, motion, useAnimate, useAnimation, useAnimationControls } from "framer-motion";
 import { atom, useAtom, useAtomValue, useSetAtom } from 'jotai';
 import * as Atoms from '@/lib/utils/atoms.js';
 import { useHotkeys } from "react-hotkeys-hook";
+import { HotKeys } from 'react-hotkeys'
 import PositionedComponent from "./PositionedComponent";
 import { FrequencyChange } from '@/utils/constants.js';
 import useRandomNode from '@/lib/hooks/useRandomNode.js';
+import _ from 'lodash'
+// import { animate } from "motion";
 
 const isHoveredAtom = atom(false)
 const isFlippedAtom = atom(false)
@@ -40,9 +43,10 @@ export default function NodeCard() {
 
     useEffect(() => {
         if (isFlipped || isHovered) {
+
             timerAnimation.stop()
         } else {
-            timerAnimation.start(targetStyles)
+            // timerAnimation.start(targetStyles)
         }
     }, [isFlipped, isHovered, duration])
 
@@ -52,15 +56,7 @@ export default function NodeCard() {
         console.log("NodeCard nodeID", currentNode?.id, "duration:", currCardDuration, "timleine idx:", currentTimelineIndex)
     }, [currentNode]);
 
-    useHotkeys('ctrl+d', (e) => {
-        e.preventDefault()
-        removeNode(currentNode.id)
-    })
-    
-    useHotkeys('ctrl+r', (e) => {
-        e.preventDefault()
-        resetNodeFrequencies()
-    })
+
 
     function getCurrentNodeCardDuration(wordsPerMinute = 60) {
         let minTime = 3
@@ -77,19 +73,22 @@ export default function NodeCard() {
         return _.round(Math.max(readingSpeedInSeconds, minTime), 2)
     }
 
-    const halfRotationDuration = .125
+    
     const rotationAnimation = useAnimation()
 
     const handleClick = (e) => {
         if (e.target.id !== "node-card") return;
+        flipNodeCardOver();
+    }
 
+    function flipNodeCardOver() {
+        const halfRotationDuration = .125
         rotationAnimation.start({
             rotateY: 90,
             transition: { duration: halfRotationDuration },
             ease: "easeOut"
-
         }).then(() => {
-            setIsFlipped(!isFlipped)
+            setIsFlipped(!isFlipped);
             rotationAnimation.start({
                 rotateY: 0,
                 transition: { duration: halfRotationDuration },
@@ -97,8 +96,13 @@ export default function NodeCard() {
             });
         });
     }
+    const timerAnimation = useAnimation()
 
-    const timerAnimation = useAnimationControls()
+    const [timerScope, timerAnimationtControls] = useAnimate()
+    // const timerAnimation2 = timerAnimationtControls(
+    //     timerScope.current, { width: "0px" }, { duration: Math.max(5, duration) }
+    // )
+
     let initialStyles = {
         opacity: .15,
         width: "525px",
@@ -107,18 +111,20 @@ export default function NodeCard() {
         opacity: .2,
         width: "0px",
         transition: {
-            duration: duration,
+            duration: Math.min(5, duration),
             ease: "linear"
         }
     }
 
     function restartTimerAnimation() {
+        // timerAnimation2.cancel()
+        // timerAnimation2.play()
         timerAnimation.stop()
         timerAnimation.set(initialStyles)
         timerAnimation.start(targetStyles)
     }
 
-    async function onNextCardCliked() {
+    function onNextCardCliked() {
         if (currentNode == null) { return }
         let randNode;
         do {
@@ -134,14 +140,8 @@ export default function NodeCard() {
         restartTimerAnimation()
     }
 
-    let TimerBar =
-        <StyledTimerBar
-            $isVisible={!isFlipped}
-            $isHovered={isHovered}
-            animate={timerAnimation}
-            initial={initialStyles}
-            onAnimationComplete={async () => { await onNextCardCliked() }}
-        />
+
+
 
     let CardControls =
         <div className="card-controls" >
@@ -157,15 +157,15 @@ export default function NodeCard() {
             </IconButton>
             {isFlipped && <div>
                 <IconButton className="nav-btn bottom left outlined"
-                    onClick={() => { 
-                        downDistributeFrequency(currentNode.id) 
+                    onClick={() => {
+                        downDistributeFrequency(currentNode.id)
                     }}
                 >
                     <ArrowDropDownIcon />
                 </IconButton>
                 <IconButton className="nav-btn bottom right outlined"
-                    onClick={() => { 
-                        upDistributeFrequency(currentNode.id) 
+                    onClick={() => {
+                        upDistributeFrequency(currentNode.id)
                     }}
                 >
                     <ArrowDropUpIcon />
@@ -191,37 +191,62 @@ export default function NodeCard() {
             </StyledCardSide>
         </div>
 
-
+    const nodeCardKeyMap = {
+        reset: 'ctrl+r',
+        delete: 'ctrl+d',
+        flip: 'ctrl+f',
+        prev: ['left','ctrl+a'],
+        next: ['right', 'ctrl+d'],
+    }
+    const nodeCardHandlers = {
+        'reset': (e) => { e.preventDefault();  resetNodeFrequencies()},
+        'delete': (e) => async () => { await removeNode(currentNode.id)},
+        'flip': (e) => { e.preventDefault(); flipNodeCardOver()},
+        'prev': (e) => { onPrevCardClicked() },
+        'next': (e) =>  { onNextCardCliked() } ,
+    }
     return (
-        <PositionedComponent
-            id="positioned-component"
-            position="middle-center">
-            <StyledNodeCard
-                id="node-card" $isHovered={isHovered} tabIndex='-1'
-                onClick={ e => handleClick(e)}
-                onMouseEnter={() => { setIsHovered(true) }}
-                onMouseLeave={() => { setIsHovered(false) }}
-                animate={rotationAnimation}
-            >
-                {TimerBar}
-                {CardControls}
-                {CardContent}
-            </StyledNodeCard>
-        </PositionedComponent>
+        <HotKeys keyMap={nodeCardKeyMap} handlers={nodeCardHandlers} focused={"true"}>
+            <PositionedComponent
+                id="positioned-component"
+                position="middle-center">
+                <StyledMotionNodeCard
+                    id="node-card" $isHovered={isHovered} tabIndex='-1'
+                    onClick={e => handleClick(e)}
+                    onMouseEnter={() => { setIsHovered(true) }}
+                    onMouseLeave={() => { setIsHovered(false) }}
+                    animate={rotationAnimation}
+                >
+                    <StyledMotionTimerBar className="timer-bar"
+                        $isVisible={!isFlipped}
+                        $isHovered={isHovered}
+                        animate={timerAnimation}
+                        initial={initialStyles}
+                        onAnimationComplete={async () => { await onNextCardCliked() }}
+                    />
+                    {CardControls}
+                    {CardContent}
+                </StyledMotionNodeCard>
+            </PositionedComponent>
+        </HotKeys>
     );
 }
 
-const StyledTimerBar = styled(motion.div)`
+const StyledMotionTimerBar = styled(motion.div)`
+    width: 100%;
+    opacity: .15;
     position: absolute;
-    left: 50%;
-    transform: translate(-50%, 0);
     border-radius: 2px;
-    bottom: .5px;
     filter: ${props => props.$isVisible ? "none" : (props.$isHovered ? "blur(3px)" : "blur(9px)")};
     pointer-events: none;
     height: 3px;
     margin: 0 auto;
     background-color: white;
+
+    top: 100%; 
+    left: 50%; 
+    transform: translate(-50%, -3px); 
+    transform-origin: center center;
 `
 
 const StyledCardSide = styled.div`
@@ -234,7 +259,7 @@ const StyledCardSide = styled.div`
     pointer-events: none;
 `
 
-const StyledNodeCard = styled(motion.div)`
+const StyledMotionNodeCard = styled(motion.div)`
     background: #00219708;
     border-radius: 5px;
     box-shadow: 0px 0px 4px #CCC;
@@ -303,3 +328,5 @@ const StyledNodeCard = styled(motion.div)`
 
     .bottom { bottom: 10px; }
 `;
+
+
