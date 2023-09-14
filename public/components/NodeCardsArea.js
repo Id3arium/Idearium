aa998ab2c7886745238a843211553b220bd57ca7
Alex C <andres.al.campos@gmail.com>
2023-09-13 22:16:09 -0700

    fixing missing hotkey functions

--- public/components/NodeCardsArea.js
+++ public/components/NodeCardsArea.js
@@ -4,15 +4,22 @@ import NodeCard from "./NodeCard.js";
 import _ from "lodash";
 import { useEffect ,useRef} from "react";
 import * as API from '@/lib/utils/api.js'
-import { clientNodesAtom, onNextNodeAtom } from "@/lib/utils/atoms.js";
+import * as Atoms from '@/lib/utils/atoms.js';
+
 import { useAtom, useSetAtom, useAtomValue } from "jotai";
 import useRandomNode from '@/lib/hooks/useRandomNode.js'
+import useNodeCardLogic from '@/lib/hooks/useNodeCardLogic.js'
+import { HotKeys } from "react-hotkeys";
 
 export default function NodeCardsArea() {
     const hasAddedFirstTimelineNode = useRef(false);
+    const { actions, state } = useNodeCardLogic();    
 
-    const [clientNodes, clientNodesNodes] = useAtom(clientNodesAtom)
-    const onNextNode = useSetAtom(onNextNodeAtom)
+
+    const [clientNodes, clientNodesNodes] = useAtom(Atoms.clientNodesAtom)
+    const onNextNode = useSetAtom(Atoms.onNextNodeAtom)
+    const removeNode = useSetAtom(Atoms.removeNodeAtom)
+    const resetNodeFrequencies = useSetAtom(Atoms.resetNodeFrequenciesAtom)
     // const nodesCount = useAtomValue(nodesCountAtom)
     const getRandomNode = useRandomNode();
 
@@ -40,10 +47,27 @@ export default function NodeCardsArea() {
             }
         }
     }, [clientNodes])
+
+    const nodeCardKeyMap = {
+        flip: 'f',
+        prev: ['left'],
+        next: ['right'],
+        reset: 'ctrl+r',
+        delete: 'ctrl+d',
+    }
+    const nodeCardHandlers = {
+        // 'flip': (e) => {e.preventDefault(); actions.flipNodeCard()},
+        'prev': (e) => {e.preventDefault(); actions.onPrevCardClicked()},
+        'next': (e) => {e.preventDefault(); actions.onNextCardCliked()} ,
+        'reset': (e) => {e.preventDefault(); resetNodeFrequencies()},
+        'delete': async (e) => {e.preventDefault(); await removeNode(state.currentNode)},
+    }
     
     return (
         <StyledNodeCardsArea id="node-cards-area">
-            <NodeCard />
+            <HotKeys keyMap={nodeCardKeyMap} handlers={nodeCardHandlers} focused="true">
+                <NodeCard />
+            </HotKeys>
         </StyledNodeCardsArea>
     );
 }
