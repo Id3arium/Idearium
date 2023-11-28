239eefa8b127f7bcd9f238a43756898aba2299f6
Alex C <andres.al.campos@gmail.com>
2023-11-27 21:56:03 -0800

    separating all of the logic for the internal state of the card and animations from the logic for navigating the timeline

--- public/components/nodeCard/NodeCard.js
+++ public/components/nodeCard/NodeCard.js
@@ -1,48 +1,111 @@
 'use client';
-import React from "react";
+import React, { useState, useEffect, useCallback} from "react";
+import { animate, useAnimation, useMotionValue, useTransform } from "framer-motion";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { HotKeys } from 'react-hotkeys'
 import { PositionedComponent } from "../PositionedComponent.js";
-import NodeCardControls from './NodeCardControls.js'
-import NodeCardContent from './NodeCardContent.js'
-import NodeCardTimerBar from './NodeCardTimerBar.js'
+import NodeCardControls from './NodeCardControls.js';
+import NodeCardContent from './NodeCardContent.js';
+import NodeCardTimerBar from './NodeCardTimerBar.js';
 import useNodeCardLogic from "@/lib/hooks/useNodeCardLogic.js";
+import { getNodeCardDuration } from '@/lib/utils/utils.js';
 
 export default function NodeCard() {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
     const { actions, state } = useNodeCardLogic();    
+
+    const rotationAnimation = useAnimation();
+    const timerAnimationProgress = useMotionValue(0); 
+
+    const startTimerAnimation = useCallback(
+    (currNodeDuration) => {
+        const duration =  currNodeDuration;
+        animate(timerAnimationProgress, 1, { duration: duration });
+    }, [timerAnimationProgress])
+
+    const pauseTimerAnimation = useCallback(
+    () => {
+        timerAnimationProgress.destroy();
+    }, [timerAnimationProgress])
+
+    const restartTimerAnimation = useCallback(
+    (duration) => {
+        timerAnimationProgress.set(0);
+        startTimerAnimation(duration);
+    }, [timerAnimationProgress, startTimerAnimation]);
+
+    useEffect( 
+    () => {
+        if (state.currentNode == null) return
+        restartTimerAnimation(getNodeCardDuration(state.currentNode));
+    }, [state.currentNode, restartTimerAnimation]);
+
+    useEffect(
+    () => {
+        if (state.currentNode == null) return; 
+        var shouldPauseTimerBar = isFlipped || isHovered;
+        if (shouldPauseTimerBar && timerAnimationProgress.isAnimating()) {
+            pauseTimerAnimation();
+        } 
+        if(!shouldPauseTimerBar && !timerAnimationProgress.isAnimating()){
+            startTimerAnimation(getNodeCardDuration(state.currentNode));
+        }
+    }, [state.currentNode, isHovered, isFlipped, timerAnimationProgress, pauseTimerAnimation, startTimerAnimation]);
+
+    const flipNodeCard = useCallback(
+    async () => {
+        const halfRotationDuration = .1;
+        await rotationAnimation.start({
+            rotateY: 90,
+            transition: {
+                duration: halfRotationDuration,
+                ease: "easeOut"
+            },
+        });
+        setIsFlipped(!isFlipped);
+        await rotationAnimation.start({
+            rotateY: 0,
+            transition: {
+                duration: halfRotationDuration,
+                ease: "easeIn"
+            },
+        });
+    }, [rotationAnimation, isFlipped, setIsFlipped]);
+
+    const handleClick = async (e) => { if (e.target.id === "node-card") { await flipNodeCard(); } }
 
     return (
         <PositionedComponent
             id="positioned-component"
             position="middle-center">
             <StyledMotionNodeCard
-                id="node-card" $isHovered={state.isHovered} tabIndex='-1'
-                onClick={e => { actions.handleClick(e) }}
-                onMouseEnter={() => { actions.setIsHovered(true) }}
-                onMouseLeave={() => { actions.setIsHovered(false) }}
-                animate={state.rotationAnimation}
+                id="node-card" $isHovered={isHovered} tabIndex='-1'
+                onClick={e => { handleClick(e) }}
+                onMouseEnter={() => { setIsHovered(true) }}
+                onMouseLeave={() => { setIsHovered(false) }}
+                animate={rotationAnimation}
             >
                 <NodeCardTimerBar
-                    isFlipped={state.isFlipped}
-                    isHovered={state.isHovered}
-                    progress={state.timerAnimationProgress}
+                    isFlipped={isFlipped}
+                    isHovered={isHovered}
+                    progress={timerAnimationProgress}
                     onNextCardCliked={actions.onNextCardCliked}
-                    animation={state.timerAnimation}
-                    initialStyles={state.initialStyles}
+                    // animation={timerAnimation}
+                    // initialStyles={state.initialStyles}
                 />
                 <NodeCardControls
                     node={state.currentNode}
                     onNextCardCliked={actions.onNextCardCliked}
                     onPrevCardClicked={actions.onPrevCardClicked}
-                    isFlipped={state.isFlipped}
+                    isFlipped={isFlipped}
                     upDistributeFrequency={actions.upDistributeFrequency}
                     downDistributeFrequency={actions.downDistributeFrequency}
                 />
                 <NodeCardContent
                     node={state.currentNode}
-                    isFlipped={state.isFlipped}
-                    isHovered={state.isHovered}
+                    isFlipped={isFlipped}
+                    isHovered={isHovered}
                     currentTimelineIndex={state.currentTimelineIndex}
                     nodeIDsTimelineLength={state.nodeIDsTimelineLength}
                 />
