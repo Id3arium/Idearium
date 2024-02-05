e116c35e09365fac222d4566b19fb0accf3aff87
Alex C <andres.al.campos@gmail.com>
2024-02-04 21:07:46 -0800

    Moved signedn in page contet to its own component

---
+++ app/SignedInPage.js
@@ -0,0 +1,74 @@
+"use client";
+import { useEffect } from "react";
+
+import NodeCardsArea from "@/components/NodeCardsArea.js";
+import IdeaCompositionArea from "@/components/IdeaCompositionArea.js";
+import NodeCardsLibrary from "@/components/nodeCardLibrary/NodeCardsLibrary";
+import useNodeCardLogic from "@/lib/hooks/useNodeCardLogic.js";
+import ForceGraph3D from "@/components/graph/ForceGraph3DWrapper.js";
+import UserDashboard from "@/components/UserDashboard.js";
+import { GlobalHotKeys } from "react-hotkeys";
+
+import { useUser } from "@clerk/nextjs";
+import { userAtom } from "@/lib/utils/atoms.js";
+import { useSetAtom } from 'jotai';
+
+
+export function SignedInPage() {
+    const { user } = useUser();
+    const setUser = useSetAtom(userAtom);
+
+    useEffect(() => {
+        setUser(user);
+    }, [user, setUser]);
+
+    const { actions, state } = useNodeCardLogic();
+    const nodeCardKeyMap = {
+        flip: "ctrl+f",
+        prev: ["left"],
+        next: ["right"],
+        reset: "ctrl+r",
+        delete: "ctrl+d"
+    };
+    const nodeCardHandlers = {
+        // 'flip': async(e) => {e.preventDefault(); await actions.flipNodeCard()},
+        prev: (e) => {
+            e.preventDefault();
+            actions.onPrevCardClicked();
+        },
+        next: (e) => {
+            e.preventDefault();
+            actions.onNextCardCliked();
+        },
+        reset: (e) => {
+            e.preventDefault();
+            resetNodeFrequencies();
+        },
+        delete: async (e) => {
+            e.preventDefault();
+            await removeNode(state.currentNode);
+        }
+    };
+    return <main className="">
+        <GlobalHotKeys
+            keyMap={nodeCardKeyMap}
+            handlers={nodeCardHandlers}
+            focused="true"
+        >
+            <div id="Home">
+                <ForceGraph3D id="ForceGraph3D" />
+                <div
+                    id="left-content"
+                    className="absolute top-0 left-0"
+                >
+                    <IdeaCompositionArea />
+                    <UserDashboard />
+                </div>
+                <NodeCardsLibrary />
+                <NodeCardsArea />
+                <div className="relative"></div>
+                {/* <NodeTimeline /> */}
+            </div>
+        </GlobalHotKeys>
+    </main>;
+}
--- app/page.js
+++ app/page.js
@@ -1,76 +1,18 @@
 "use client";
 import "@/app/styles.css";
-import NodeCardsArea from "@/components/NodeCardsArea.js";
-import IdeaCompositionArea from "@/components/IdeaCompositionArea.js";
-import NodeCardsLibrary from "@/components/nodeCardLibrary/NodeCardsLibrary";
-import useNodeCardLogic from "@/lib/hooks/useNodeCardLogic.js";
-
-import { GlobalHotKeys } from "react-hotkeys";
-import {ClerkProvider, SignedIn, SignedOut, currentUser, SignIn, SignOutButton,} from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import { useUser } from "@clerk/clerk-react";
-
 import ForceGraph3D from "@/components/graph/ForceGraph3DWrapper.js";
-import UserDashboard from "@/components/UserDashboard.js";
-export default function Home() {
 
-    const { actions, state } = useNodeCardLogic();
-    const nodeCardKeyMap = {
-        flip: "ctrl+f",
-        prev: ["left"],
-        next: ["right"],
-        reset: "ctrl+r",
-        delete: "ctrl+d",
-    };
-    const nodeCardHandlers = {
-        // 'flip': async(e) => {e.preventDefault(); await actions.flipNodeCard()},
-        prev: (e) => {
-            e.preventDefault();
-            actions.onPrevCardClicked();
-        },
-        next: (e) => {
-            e.preventDefault();
-            actions.onNextCardCliked();
-        },
-        reset: (e) => {
-            e.preventDefault();
-            resetNodeFrequencies();
-        },
-        delete: async (e) => {
-            e.preventDefault();
-            await removeNode(state.currentNode);
-        },
-    };
+import { ClerkProvider, SignedIn, SignedOut, useUser, SignIn, SignOutButton} from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
+import { SignedInPage } from "./SignedInPage";
+export default function Home() {  
     return (
         <ClerkProvider>
-            <SignedIn>
-                <main class="">
-                    <GlobalHotKeys
-                        keyMap={nodeCardKeyMap}
-                        handlers={nodeCardHandlers}
-                        focused="true"
-                    >
-                        <div id="Home">
-                            <ForceGraph3D id="ForceGraph3D" />
-                            <div id="left-content" className="absolute top-0 left-0">
-                                <IdeaCompositionArea />
-                                <UserDashboard/>
-                            </div>
-                            <NodeCardsLibrary />
-                            <NodeCardsArea />
-                            <div className="relative">
-                                
-                            </div>
-                            {/* <NodeTimeline /> */}
-                        </div>
-                    </GlobalHotKeys>
-                </main>
-            </SignedIn>
             <SignedOut>
-                <div class="relative w-screen h-screen">
+                <div className="relative w-screen h-screen">
                     <ForceGraph3D id="ForceGraph3D" />
-                    <div class="absolute left-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <div className="absolute left-1/3 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <SignIn
                             appearance={{
                                 baseTheme: dark,
@@ -87,6 +29,10 @@ export default function Home() {
                     </div>
                 </div>
             </SignedOut>
+            <SignedIn>
+                <SignedInPage />
+            </SignedIn>
         </ClerkProvider>
     );
 }
+
