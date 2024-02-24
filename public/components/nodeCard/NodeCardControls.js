870bbd1569bf6ced1f91b0c40d559759cf1df124
Alejandro <andres.al.campos@gmail.com>
2024-02-23 21:22:29 -0800

    updated styles to tailwind

--- public/components/nodeCard/NodeCardControls.js
+++ public/components/nodeCard/NodeCardControls.js
@@ -1,62 +1,60 @@
-
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import { IconButton } from "@mui/material";
 import styled from "@emotion/styled";
 
-export default function NodeCardControls({ onPrevCardClicked, onNextCardCliked, isFlipped, downDistributeFrequency, node, upDistributeFrequency }) {
+export default function NodeCardControls({
+    onPrevCardClicked,
+    onNextCardCliked,
+    isFlipped,
+    isHovered,
+    downDistributeFrequency,
+    node,
+    upDistributeFrequency,
+}) {
     return (
-        <StyledNodeCardControls id="card-controls">
-            <IconButton className="nav-btn top left outlined"
-                onClick={() => { onPrevCardClicked(); }}
+        <div
+            id="card-controls"
+            className={`${isHovered ? "block" : "hidden"} relative`}
+        >
+            <button
+                className="absolute w-9 h-9 top-3 left-3 rounded-[50%] z-10 outline-none  hover:outline-1 hover:outline-white/50"
+                onClick={() => {
+                    onPrevCardClicked();
+                }}
             >
-                <KeyboardArrowLeftIcon />
-            </IconButton>
-            <IconButton className="nav-btn top right outlined"
-                onClick={() => { onNextCardCliked(); }}
+                <KeyboardArrowLeftIcon className="text-white -m-right-1" />
+            </button>
+            <button
+                className="absolute w-9 h-9 top-4 right-4 rounded-[50%] z-10 outline-none  hover:outline-1 hover:outline-white/50"
+                onClick={() => {
+                    onNextCardCliked();
+                }}
             >
-                <KeyboardArrowRightIcon disabled={true} />
-            </IconButton>
-            {isFlipped && <div>
-                <IconButton className="nav-btn bottom left outlined"
-                    onClick={() => {
-                        downDistributeFrequency(node.id);
-                    }}
-                >
-                    <ArrowDropDownIcon />
-                </IconButton>
-                <IconButton className="nav-btn bottom right outlined"
-                    onClick={() => {
-                        upDistributeFrequency(node.id);
-                    }}
-                >
-                    <ArrowDropUpIcon />
-                </IconButton>
-            </div>}
-        </StyledNodeCardControls>
-    )
+                <KeyboardArrowRightIcon className="text-white" />
+            </button>
+            {isFlipped && (
+                <div className="absolute inset-x-0 bottom-2 z-10 flex justify-between">
+                    <button
+                        className="nav-btn outline-none hover:outline hover:outline-1 hover:outline-white/50"
+                        onClick={() => {
+                            downDistributeFrequency(node.id);
+                        }}
+                    >
+                        <ArrowDropDownIcon className="text-white mx-auto" />
+                    </button>
+                    <button
+                        className="nav-btn outline-none hover:outline hover:outline-1 hover:outline-white/50"
+                        onClick={() => {
+                            upDistributeFrequency(node.id);
+                        }}
+                    >
+                        <ArrowDropUpIcon className="text-white" />
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 }
-
-const StyledNodeCardControls = styled.div(`    
-    display: none;
-    
-    .nav-btn {
-        color: white;
-        position: absolute;
-        z-index: 1;
-    }
-
-    .outlined:hover {
-        outline: 1px solid #ffffff80;
-    }
-    
-    .left { left: 10px; }
-
-    .right { right: 10px; }
-
-    .top { top: 10px; }
-    
-    .bottom { bottom: 10px; }
-`)
