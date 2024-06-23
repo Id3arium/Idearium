308a27e4fe3512e645525cf6dca39c58afb8ebbd
Alejandro <andres.al.campos@gmail.com>
2024-06-22 20:55:39 -0700

    making up/down controls persistent, changing order of controls

--- public/components/nodeCard/NodeCardControls.js
+++ public/components/nodeCard/NodeCardControls.js
@@ -2,60 +2,62 @@ import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import EditIcon from "@mui/icons-material/Edit";
 
 export default function NodeCardControls({
     node,
     actions,
     isFlipped,
     isHovered,
-    onMouseEnter,
-    onMouseLeave,
+    onEditCardClicked,
 }) {
     return (
         <div
             id="card-controls"
-            className={`${ 
+            className={`${
                 isHovered || isFlipped ? "opacity-100" : "opacity-0"
-            } relative left-0 right-0 px-4 w-[525px] h-[50px]`}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
+            } relative left-0 right-0 py-2 w-full h-[50px]`}
         >
             <button
-                className="absolute w-9 h-9 left-0 rounded-[50%] z-10 outline-none  hover:outline-1 hover:outline-white/50"
+                className="relative w-9 h-9 mx-2 rounded-full outline-none hover:outline hover:outline-1 hover:outline-white/50"
+                onClick={() => {
+                    actions.downDistributeFrequency(node.id);
+                }}
+            >
+                <ArrowDropDownIcon className="text-white mx-auto" />
+            </button>
+            <button
+                className="relative w-9 h-9 mx-2 rounded-full outline-none hover:outline hover:outline-1 hover:outline-white/50"
+                onClick={() => {
+                    actions.upDistributeFrequency(node.id);
+                }}
+            >
+                <ArrowDropUpIcon className="text-white" />
+            </button>
+            <button
+                className="relative w-9 h-9 mx-2 rounded-[50%] z-10 outline-none hover:outline-1 hover:outline-white/50"
                 onClick={() => {
                     actions.onPrevCardClicked();
                 }}
             >
-                <KeyboardArrowLeftIcon className="text-white -m-right-1" />
+                <KeyboardArrowLeftIcon className="text-white" />
             </button>
             <button
-                className="absolute w-9 h-9 right-0 rounded-[50%] z-10 outline-none  hover:outline-1 hover:outline-white/50"
+                className="relative w-9 h-9 mx-2 rounded-[50%] z-10 outline-none hover:outline-1 hover:outline-white/50"
                 onClick={() => {
                     actions.onNextCardCliked();
                 }}
             >
                 <KeyboardArrowRightIcon className="text-white" />
             </button>
-            {isFlipped && (
-                <div className="absolute inset-x-0 bottom-2 z-10 flex justify-between">
-                    <button
-                        className="nav-btn outline-none hover:outline hover:outline-1 hover:outline-white/50"
-                        onClick={() => {
-                            actions.downDistributeFrequency(node.id);
-                        }}
-                    >
-                        <ArrowDropDownIcon className="text-white mx-auto" />
-                    </button>
-                    <button
-                        className="nav-btn outline-none hover:outline hover:outline-1 hover:outline-white/50"
-                        onClick={() => {
-                            actions.upDistributeFrequency(node.id);
-                        }}
-                    >
-                        <ArrowDropUpIcon className="text-white" />
-                    </button>
-                </div>
-            )}
+            <button
+                className="absolute w-9 h-9 right-2 rounded-full z-10 outline-none hover:outline hover:outline-1 hover:outline-white/50"
+                onClick={() => {
+                    onEditCardClicked();
+                }}
+            >
+                <EditIcon className="text-white" />
+            </button>
         </div>
     );
 }
