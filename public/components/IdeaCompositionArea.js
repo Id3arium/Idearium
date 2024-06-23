8b926304365b4f7ff95ce75caf73de162a0648f2
Alejandro <andres.al.campos@gmail.com>
2024-06-22 20:36:37 -0700

    title bar focuses when expanding idea comp area. cleaned up component

--- public/components/IdeaCompositionArea.js
+++ public/components/IdeaCompositionArea.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
@@ -14,6 +14,8 @@ function IdeaCompositionArea() {
         inspiration: "",
     });
     const [isExpanded, setIsExpanded] = useState(false);
+    const titleInputRef = useRef(null);
+
     const createNode = useCreateNode();
     const addNode = useSetAtom(addNodeAtom);
 
@@ -35,90 +37,80 @@ function IdeaCompositionArea() {
         }
         resetForm();
     }
+    useEffect(() => {
+        if (isExpanded) {
+            titleInputRef.current?.focus();
+        }
+    }, [isExpanded]);
 
     return (
-        <div className="w-[400px] m-5">
-            <form
-                onSubmit={handleSubmit}
-                className={`
-                    relative
+        <form
+            onSubmit={handleSubmit}
+            className={`
+                relative overflow-visible w-[400px] m-5
+                transition-all duration-250 ease-in-out 
+                focus:outline-none focus:ring-0 
+                bg-clear rounded-lg shadow-[0_0_6px_#CCC] backdrop-blur-[9px]
+                ${isExpanded ? "h-[200px]" : "h-[44px]"}
+            `}
+        >
+            <div
+                id="note-input-bars"
+                className={`absolute top-0 left-0 w-full 
                     transition-all duration-250 ease-in-out 
-                    focus:outline-none focus:ring-0 
-                    bg-clear rounded-lg shadow-[0_0_6px_#CCC] backdrop-blur-[9px]
-                    overflow-visible
-                    ${isExpanded ? "h-[200px]" : "h-[44px]"}
+                    ${isExpanded ? "opacity-100" : "opacity-0"}
                 `}
             >
-                <div
-                    className={`
-                    absolute top-0 left-0 w-full
-                    transition-all duration-250 ease-in-out
-                    ${
-                        isExpanded
-                            ? "opacity-100 translate-y-0"
-                            : "opacity-0 -translate-y-2"
-                    }
-                `}
-                >
-                    <input
-                        type="text"
-                        name="title"
-                        className="w-full bg-clear p-2.5 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
-                        placeholder="Title (Optional)"
-                        value={note.title}
-                        onChange={handleInputChange}
-                    />
-                    <textarea
-                        name="content"
-                        className="w-full bg-clear p-2.5 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
-                        rows="3"
-                        placeholder="Content"
-                        value={note.content}
-                        onChange={handleInputChange}
-                    />
-                    <input
-                        type="text"
-                        name="inspiration"
-                        className="w-full bg-clear p-2.5 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
-                        placeholder="Inspiration"
-                        value={note.inspiration}
-                        onChange={handleInputChange}
-                    />
-                </div>
                 <input
                     type="text"
-                    className={`
-                        absolute top-0 left-0 w-full
-                        bg-clear p-2.5 text-white rounded-lg
-                        transition-all duration-250 ease-in-out
-                        ${
-                            isExpanded
-                                ? "opacity-0 translate-y-2 pointer-events-none"
-                                : "opacity-100 translate-y-0"
-                        }
-                    `}
-                    placeholder="Compose An Idea..."
-                    onFocus={() => setIsExpanded(true)}
+                    name="title"
+                    className="w-full bg-clear p-2.5 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
+                    placeholder="Title (Optional)"
+                    ref={titleInputRef}
+                    value={note.title}
+                    onChange={handleInputChange}
                 />
-                <div
-                    className={`
-                    absolute -bottom-5 right-0
-                    transition-all duration-250 ease-in-out
-                    ${
-                        isExpanded
-                            ? "opacity-100 translate-y-0"
-                            : "opacity-0 translate-y-2 pointer-events-none"
-                    }
+                <textarea
+                    name="content"
+                    className="w-full bg-clear p-2.5 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
+                    rows="3"
+                    placeholder="Content"
+                    value={note.content}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="text"
+                    name="inspiration"
+                    className="w-full bg-clear p-2.5 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
+                    placeholder="Inspiration"
+                    value={note.inspiration}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <input
+                type="text"
+                id="default-input-bar"
+                className={` absolute top-0 left-0 w-full bg-clear p-2.5 text-white
+                    rounded-lg transition-all duration-250 ease-in-out
+                    ${isExpanded ? "opacity-0 pointer-events-none" : "opacity-100"}
                 `}
-                >
-                    <Zoom in={isExpanded}>
-                        <Fab size="small" type="submit">
-                            <AddIcon />
-                        </Fab>
-                    </Zoom>
-                </div>
-            </form>
-        </div>
+                placeholder="Compose An Idea..."
+                onChange={() => setIsExpanded(true)}
+                onFocus={() => setIsExpanded(true)}
+            />
+            <div
+                className={`absolute -bottom-5 right-0 
+                    transition-all duration-250 ease-in-out
+                    ${isExpanded ? "opacity-100" : "opacity-0 pointer-events-none"}
+                `}
+            >
+                <Zoom in={isExpanded}>
+                    <Fab size="small" type="submit">
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
+            </div>
+        </form>
     );
 }
 
