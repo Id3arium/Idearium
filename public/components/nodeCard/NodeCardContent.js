c90caaf62464d412c195c2efc2d87c37278c8ea1
Alejandro <andres.al.campos@gmail.com>
2024-06-22 20:55:43 -0700

    adding input fields for when content is being edited. wip

--- public/components/nodeCard/NodeCardContent.js
+++ public/components/nodeCard/NodeCardContent.js
@@ -1,18 +1,15 @@
 export default function NodeCardContent({
+    node,
     isFlipped,
     isHovered,
-    node,
+    isEditing,
     currentTimelineIndex,
     nodeIDsTimelineLength,
 }) {
     function getConditionalStyles(isVisible) {
         const opacityClass = isVisible ? "opacity-100" : "opacity-35";
         const scaleClass = isVisible ? "scale-x-[1]" : "scale-x-[-1]";
-        const blurClass = isVisible
-            ? ""
-            : isHovered
-            ? "blur-sm"
-            : "blur-lg";
+        const blurClass = isVisible ? "" : isHovered ? "blur-sm" : "blur-lg";
         return `${opacityClass} ${scaleClass} ${blurClass}`;
     }
 
@@ -21,16 +18,52 @@ export default function NodeCardContent({
             id="card-content"
             className="grid text-center text-md-lg p-7 pointer-events-none select-none"
         >
-            <div
+            {/* <div
                 id="front-side"
                 className={`col-start-1 row-start-1 flex flex-col items-center justify-center transform -translate-y-0.5 ${getConditionalStyles(!isFlipped)}`}
             >
                 {node?.title && <h1>{node?.title}</h1>}
                 <p>{node?.content}</p>
+            </div> */}
+            <div
+                id="front-side"
+                className={`col-start-1 row-start-1 flex flex-col items-center justify-center transform -translate-y-0.5 ${getConditionalStyles(
+                    !isFlipped
+                )}`}
+            >
+                {isEditing ? (
+                    <div className="">
+                        <input
+                            type="text"
+                            className="w-full bg-clear p-1 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
+                            placeholder={node?.title || "Title"}
+                            value={node?.title || ""}
+                            onChange={(e) => {
+                                onInputChanged(e);
+                            }}
+                        />
+                        <textarea
+                            className="w-full bg-clear p-1 text-white focus:outline-none focus:ring-0 focus:ring-grey-dark/50 rounded-md"
+                            // rows="4"
+                            placeholder={node?.content || "Content"}
+                            value={node?.content || ""}
+                            onChange={(e) => {
+                                onInputChanged(e);
+                            }}
+                        />
+                    </div>
+                ) : (
+                    <>
+                        {node?.title && <h1>{node?.title}</h1>}
+                        <p>{node?.content}</p>
+                    </>
+                )}
             </div>
             <div
                 id="back-side"
-                className={`col-start-1 row-start-1 ${getConditionalStyles(isFlipped)}`}
+                className={`col-start-1 row-start-1 ${getConditionalStyles(
+                    isFlipped
+                )}`}
             >
                 <h1 className="mb-4 text-lg text-center">
                     Node [{currentTimelineIndex + 1} / {nodeIDsTimelineLength}]
